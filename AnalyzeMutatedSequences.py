5aa244d957ceca264f08b9cab25491c78e9b51ca
Kyle Meador <kylemeador@g.ucla.edu>
2020-07-26 08:35:40 -1000

    fix energy_s selection

--- AnalyzeMutatedSequences.py
+++ AnalyzeMutatedSequences.py
@@ -1022,7 +1022,7 @@ def select_sequences(des_dir, number=1, debug=False):
         except ValueError:
             raise SDUtils.DesignError('no dataframe')
         energy_s.sort_values(inplace=True)
-        final_seqs = zip(repeat(des_dir.path), energy_s.iloc[:number, :].index.to_list())
+        final_seqs = zip(repeat(des_dir.path), energy_s.iloc[:number].index.to_list())  # , :].index.to_list())
     else:
         final_seqs = zip(repeat(des_dir.path), final_designs.keys())
 
