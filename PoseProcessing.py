442cced6dd42d0e53d0ca09b4de892da6327bb3e
Kyle Meador <kylemeador@g.ucla.edu>
2020-07-30 17:29:24 -1000

    debug identity value generation

--- PoseProcessing.py
+++ PoseProcessing.py
@@ -194,6 +194,7 @@ def pose_rmsd(all_des_dirs):
                 else:
                     pose_map[pair[0].building_blocks][str(pair[0])] = {str(pair[1]): pair_rmsd}
                     pose_map[pair[0].building_blocks][str(pair[0])][str(pair[0])] = 0.0
+                    pose_map[pair[0].building_blocks][str(pair[1])][str(pair[1])] = 0.0
                 # pose_map[pair[0].building_blocks][(str(pair[0]), str(pair[1]))] = pair_rmsd[2]
             else:
                 pose_map[pair[0].building_blocks] = {str(pair[0]): {str(pair[1]): pair_rmsd}}
