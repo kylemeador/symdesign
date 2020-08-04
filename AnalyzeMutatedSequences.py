76e77dd6386d83dd3df7117554ce4c7e5b8279e5
Kyle Meador <kylemeador@g.ucla.edu>
2020-08-03 20:01:15 -1000

    debug generate_mutations_from_seq order

--- AnalyzeMutatedSequences.py
+++ AnalyzeMutatedSequences.py
@@ -607,20 +607,21 @@ def generate_mutations_from_seq(mutant, reference, offset=True, blanks=False, te
                 if mutations[entry]['to'] != '-':
                     remove_mutation_list.append(entry)
         termini, reference_gaps, blanks = True, True, True
-    if not termini:  # Remove indices outside of sequence 2
-        for entry in mutations:
-            if entry < 0 or entry > ending_index_of_seq2:
-                remove_mutation_list.append(entry)
-    if not reference_gaps:  # Remove indices inside sequence 2 where sequence 1 is gapped
-        for entry in mutations:
-            if entry > 0 or entry <= ending_index_of_seq2:
-                if mutations[entry]['to'] == '-':
-                    remove_mutation_list.append(entry)
     if not blanks:  # Remove any blank mutations
         for entry in mutations:
             for index in mutations[entry]:
                 if mutations[entry][index] == '-':
                     remove_mutation_list.append(entry)
+    else:
+        if not termini:  # Remove indices outside of sequence 2
+            for entry in mutations:
+                if entry < 0 or entry > ending_index_of_seq2:
+                    remove_mutation_list.append(entry)
+        if not reference_gaps:  # Remove indices inside sequence 2 where sequence 1 is gapped
+            for entry in mutations:
+                if entry > 0 or entry <= ending_index_of_seq2:
+                    if mutations[entry]['to'] == '-':
+                        remove_mutation_list.append(entry)
 
     for entry in remove_mutation_list:
         if entry in mutations:
