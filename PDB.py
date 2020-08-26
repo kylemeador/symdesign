8eaaf67ff8f361de24f456962732b751a3d19a52
Kyle Meador <kylemeador@g.ucla.edu>
2020-08-25 18:27:34 -0700

    modified orient executable location

--- PDB.py
+++ PDB.py
@@ -614,7 +614,8 @@ class PDB:
 
     def orient(self, symm, orient_dir, generate_oriented_pdb=True):
         os.system('cp %s input.pdb' % self.filepath)
-        os.system('%s/orient_oligomer >> orient.out 2>&1 << eof\n%s/%s_symm.txt\neof' % (orient_dir, orient_dir, symm))
+        os.system('%s/orient_oligomer_rmsd >> orient.out 2>&1 << eof\n%s/%s\neof' % (orient_dir, orient_dir, symm))
+        # os.system('%s/orient_oligomer >> orient.out 2>&1 << eof\n%s/%s_symm.txt\neof' % (orient_dir, orient_dir, symm))
         os.system('mv output.pdb %s_orient.pdb' % os.path.splitext(self.filepath)[0])
         os.system('rm input.pdb')
         if os.path.exists('%s_orient.pdb' % os.path.splitext(self.filepath)[0]):
