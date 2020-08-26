f726072fb839437a0e60ade23f99ed5c373679cf
kylemeador <kylemeador@g.ucla.edu>
2020-08-25 21:43:57 -0700

    switch wildtype and mutation sequence

--- ASU.py
+++ ASU.py
@@ -57,7 +57,7 @@ def design_recapitulation(design_file, pdb_dir, output_dir):
             used_chains += oriented_pdb.chain_id_list
             oriented_pdb_seq_a = oriented_pdb.atom_sequences[oriented_pdb.chain_id_list[0]]
             chain_in_asu = asu.match_entity_by_seq(other_seq=oriented_pdb_seq_a, force_closest=True)
-            design_mutations = Ams.generate_mutations_from_seq(oriented_pdb_seq_a, asu.atom_sequences[chain_in_asu],
+            design_mutations = Ams.generate_mutations_from_seq(asu.atom_sequences[chain_in_asu], oriented_pdb_seq_a,
                                                                blanks=True)
             for residue in design_mutations:
                 if design_mutations[residue]['from'] == '-':
