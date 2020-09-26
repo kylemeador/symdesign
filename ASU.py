8792b3e51a762f060a64c331fb86231952429da1
Kyle Meador <kylemeador@g.ucla.edu>
2020-09-25 18:14:12 -0700

    p.communicate()

--- ASU.py
+++ ASU.py
@@ -222,12 +222,14 @@ def design_recapitulation(design_file, pdb_dir, output_dir, oligomer=False):
 
 def run_rmsd_calc(design_list, design_map_pickle):
     design_map = SDUtils.unpickle(design_map_pickle)
+    logger.info('Starting RMSD calculation')
     for design in design_list:
         design = design.strip()
         rmsd_cmd = ['python', '/home/kmeador/Nanohedra/crystal_vs_docked_v2.py', design_map[design]['pdb1'],
                     design_map[design]['pdb2'], design_map[design]['nanohedra_output'],
                     design_map[design]['nanohedra_output']]
         p = subprocess.Popen(rmsd_cmd)  # , capture_output=True)
+        p.communicate()
         logger.info('%s finished RMSD calculation' % design)
 
 
