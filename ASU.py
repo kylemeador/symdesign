9aa2bfced357fb2379105e4d2234d13add50f031
Kyle Meador <kylemeador@g.ucla.edu>
2020-10-06 12:55:16 -0700

    cleaned up design_recapitulation and code notes. remove _vflip

--- ASU.py
+++ ASU.py
@@ -29,28 +29,23 @@ def make_asu_oligomer(asu, chain_map, location=os.getcwd):
 
     Args:
         asu (PDB): a PDB instance with the correctly oriented ASU for design
-        chain_map (dict): Relation of the chains in the oriented oligomer to corresponding chain name in the asu PDB &
-            the paths to the oriented oligomers
+        chain_map (dict): {'pdb1': {'asu_chain': 'A', 'dock_chains': ['A', 'B', 'C'], 'path': path/to/.pdb}, {}}
+            Relation of the chains in the oriented oligomer to corresponding chain name in the asu PDB & the paths to
+            the oriented oligomers
     Keyword Args:
         location=os.getcwd() (str): Location on disk to write the ASU oriented oligomer files
     Returns:
         (dict): {'nanohedra_output': /path/to/directory, 'pdb1': /path/to/design_asu/pdb1_oligomer.pdb, 'pdb1': ...}
             The locations of the oligomeric asu and the Nanohedra directory
     """
-    # chain_map = {'pdb1': {'asu_chain': chain_in_asu, 'dock_chains': oriented_pdb.chain_id_list, 'path': path/to/.pdb}, {}}
-    # move each oligomer to correct asu chain
-
-    # for design in chain_map:  # TODO make correspondance design specific
     moved_oligomer = {}
     for pdb in chain_map:
         asu_chain = chain_map[pdb]['asu_chain']
         oriented_oligomer = SDUtils.read_pdb(chain_map[pdb]['path'])
         oligomer_chain = chain_map[pdb]['dock_chains'][0]
-        moved_oligomer[pdb] = biopdb_aligned_chain(asu, asu_chain, oriented_oligomer, oligomer_chain) #uncomment when path is solved
-        # moved_oligomer[pdb] = None  # remove when path is solved
+        moved_oligomer[pdb] = biopdb_aligned_chain(asu, asu_chain, oriented_oligomer, oligomer_chain)
         # moved_oligomer = biopdb_aligned_chain(pdb_fixed, chain_id_fixed, pdb_moving, chain_id_moving)
     final_comparison = {'nanohedra_output': glob(os.path.join(os.path.dirname(location), 'NanohedraEntry*DockedPoses'))[0]}
-    # final_comparison = {'nanohedra_output': os.path.join(os.path.dirname(location), 'NanohedraEntry%sDockedPoses' % entry_num)}
     for pdb in moved_oligomer:
         moved_oligomer[pdb].write(os.path.join(location, '%s_oligomer.pdb' % pdb))  # design/design_asu/pdb1_oligomer.pdb
         final_comparison[pdb] = os.path.join(location, '%s_oligomer.pdb' % pdb)
@@ -58,17 +53,33 @@ def make_asu_oligomer(asu, chain_map, location=os.getcwd):
     return final_comparison
 
 
-def design_recapitulation(design_file, pdb_dir, output_dir, oligomer=False):
+def design_recapitulation(design_file, output_dir, pdb_dir=None, oligomer=False):
+    """Recapitulate a published design using Nanohedra docking
+
+    Args
+        design_file (str): Location of .csv file containing design information including:
+            design_name, pdb_code1, pdb_code2, symm1, symm2, final_symm
+        output_dir (str): Location where all files processed for Nanohedra should be written
+    Keyword Args
+        pdb_dir=None (str): Location on disk of all design .pdb files. If not provided, then the PDB will be sourced
+            from the default directory structure, i.e. output_dir/design_asus/design_name.pdb
+        oligomer=False (bool): Whether or not the output ASU should contain the oligomer (True) or just the ASU (False)
+    Returns:
+        None
+    """
     qsbio_assemblies = SDUtils.unpickle(PUtils.qsbio)
-    with open(design_file, 'r') as f:
-        reading_csv = reader(f)
-        design_file_input = {os.path.splitext(row[0])[0]:
-                             # {'design_pdb': SDUtils.read_pdb(os.path.join(pdb_dir, row[0])),  # TODO reinstate upon recap exp termination
-                             {'design_pdb': row[0],
-                              'source_pdb': [(row[1], row[3]), (row[2], row[4])], 'final_sym': row[5]}
-                             for row in reading_csv}  # 'pdb1': 'sym1': 'pdb2': 'sym2':
-    # all_pdbs = {file: {'design': SDUtils.read_pdb(pdb_file) for pdb_file in all_pdb_files} for file in design_input
-    # all_pdb_files = SDUtils.get_all_pdb_file_paths(pdb_dir)
+    with open(design_file, 'r') as f_csv:
+        reading_csv = reader(f_csv)
+        if pdb_dir:
+            design_file_input = {os.path.splitext(row[0])[0]:
+                                 {'design_pdb': SDUtils.read_pdb(os.path.join(pdb_dir, row[0])),
+                                  'source_pdb': [(row[1], row[3]), (row[2], row[4])], 'final_sym': row[5]}
+                                 for row in reading_csv}  # 'pdb1': 'sym1': 'pdb2': 'sym2':
+        else:
+            design_file_input = {os.path.splitext(row[0])[0]:
+                                     {'design_pdb': row[0], 'source_pdb': [(row[1], row[3]), (row[2], row[4])],
+                                      'final_sym': row[5]} for row in reading_csv}  # 'pdb1': 'sym1': 'pdb2': 'sym2':
+
     if not os.path.exists(os.path.join(output_dir, 'design_asus')):
         os.makedirs(os.path.join(output_dir, 'design_asus'))
     if not os.path.exists(os.path.join(output_dir, 'biological_assemblies')):
@@ -80,13 +91,13 @@ def design_recapitulation(design_file, pdb_dir, output_dir, oligomer=False):
     #           'pdb2': {'asu_chain': None, 'dock_chains': []}}, ...}
     rmsd_comp_commands = {}
     for design in design_file_input:
-        # asu = design_file_input[design]['design_pdb'].return_asu()  # TODO reinstate upon design recap exp termination
-        asu = SDUtils.read_pdb(os.path.join(output_dir, 'design_asus', design + '.pdb'))  # old when design asu is outside
-        # asu = SDUtils.read_pdb(os.path.join(output_dir, design, 'design_asus',design  + '.pdb'))  # new when asu is inside design directory
+        if pdb_dir:
+            asu = design_file_input[design]['design_pdb'].return_asu()
+        else:
+            # asu = SDUtils.read_pdb(os.path.join(output_dir, design, 'design_asus', design  + '.pdb'))  # TODO new, asu is inside design directory
+            asu = SDUtils.read_pdb(os.path.join(output_dir, 'design_asus', design + '.pdb'))  # old, design_asus outside
         asu.reorder_chains()
         # asu.pose_numbering()
-        # for chain in asu.chain_id_list:
-        #     asu.reindex_chain_residues(chain)
         asu.reindex_all_chain_residues()
         asu.get_all_entities()
 
@@ -95,22 +106,22 @@ def design_recapitulation(design_file, pdb_dir, output_dir, oligomer=False):
             os.makedirs(design_dir)
 
         used_chains, success = [], True
-        for i, (pdb, sym) in enumerate(design_file_input[design]['source_pdb'], 1):
         # for i, sym_order in enumerate(design_file_input[design]['source_pdb'], 1):
         #     pdb, sym = design_file_input[design]['source_pdb'][sym_order]
+        for i, (pdb, sym) in enumerate(design_file_input[design]['source_pdb'], 1):
             if pdb in qsbio_assemblies:
                 biological_assembly = qsbio_assemblies[pdb][0]  # find a verified biological assembly from QSBio
-                if pdb == '3E6Q':
+                if pdb == '3E6Q':  # manual override list
                     biological_assembly = 2
             else:
-                print('%s: %s is not verified in QSBio! Arbitrarily choosing assembly 1' % (design, pdb))
+                logger.warning('%s: %s is not verified in QSBio! Arbitrarily choosing assembly 1' % (design, pdb))
                 biological_assembly = 1
             new_file = SDUtils.download_pdb('%s_%s' % (pdb, biological_assembly),
                                             location=os.path.join(output_dir, 'biological_assemblies'))
             downloaded_pdb = SDUtils.read_pdb(new_file)
             oriented_pdb = downloaded_pdb.orient(sym, PUtils.orient_dir)  # , generate_oriented_pdb=False)
             if oriented_pdb.all_atoms == list():
-                print('%s failed! Skipping design %s' % (pdb, design))
+                logger.error('%s failed! Skipping design %s' % (pdb, design))
                 success = False
                 break
             # oriented_pdb.name = pdb.lower()
@@ -119,19 +130,21 @@ def design_recapitulation(design_file, pdb_dir, output_dir, oligomer=False):
                 for chain in oriented_pdb.chain_id_list:
                     oriented_pdb.reindex_chain_residues(chain)
             except AttributeError:
-                print('%s failed! Missing residue information in re-indexing' % pdb)
+                logger.error('%s failed! Missing residue information in re-indexing' % pdb)
+                success = False
+                break
             oriented_pdb.reorder_chains(exclude_chains_list=used_chains)  # redoes sequences
             used_chains += oriented_pdb.chain_id_list
             oriented_pdb_seq_a = oriented_pdb.atom_sequences[oriented_pdb.chain_id_list[0]]
             chain_in_asu = asu.match_entity_by_seq(other_seq=oriented_pdb_seq_a, force_closest=True)
-            # print('ASU\t: %s' % asu.atom_sequences[chain_in_asu])
-            # print('Orient\t: %s' % oriented_pdb_seq_a)
+            logger.debug('ASU\t: %s' % asu.atom_sequences[chain_in_asu])
+            logger.debug('Orient\t: %s' % oriented_pdb_seq_a)
             des_mutations_asu = Ams.generate_mutations_from_seq(oriented_pdb_seq_a, asu.atom_sequences[chain_in_asu],
                                                                 blanks=True)
             des_mutations_orient = Ams.generate_mutations_from_seq(asu.atom_sequences[chain_in_asu], oriented_pdb_seq_a,
                                                                    blanks=True)
-            # print('ASU: %s' % des_mutations_asu)
-            # print('Orient: %s' % des_mutations_orient)
+            logger.debug('ASU: %s' % des_mutations_asu)
+            logger.debug('Orient: %s' % des_mutations_orient)
             # Ensure that the design mutations have the right index, must be adjusted for the offset of both sequences
             asu_offset, orient_offset = 0, 0
             for residue in des_mutations_asu:
@@ -139,22 +152,22 @@ def design_recapitulation(design_file, pdb_dir, output_dir, oligomer=False):
                     asu_offset += 1
                 if des_mutations_asu[residue]['to'] == '-':
                     asu.delete_residue(chain_in_asu, residue - asu_offset)
-                    # print('Design %s: Deleted residue %d from Design ASU' % (design, residue - asu_offset))
+                    # logger.debug'Design %s: Deleted residue %d from Design ASU' % (design, residue - asu_offset))
             # for k in range(1, 34):
-            #     print(oriented_pdb.get_residue(oriented_pdb.chain_id_list[0], k).type)
-            #     print(oriented_pdb.get_residue(oriented_pdb.chain_id_list[0], k).number)
-            # print(oriented_pdb.get_residue(chain, 0).type)
-            # print(oriented_pdb.get_residue(chain, 0).number)
+            #     logger.debug(oriented_pdb.get_residue(oriented_pdb.chain_id_list[0], k).type)
+            #     logger.debug(oriented_pdb.get_residue(oriented_pdb.chain_id_list[0], k).number)
+            # logger.debug(oriented_pdb.get_residue(chain, 0).type)
+            # logger.debug(oriented_pdb.get_residue(chain, 0).number)
             for residue in des_mutations_orient:
                 # if design_mutations[residue]['to'] == '-':
                 #     asu.delete_residue(chain_in_asu, residue)
-                #     print('Design %s: Deleted residue %d from Design ASU' % (design, residue))
+                #     logger.debug('Design %s: Deleted residue %d from Design ASU' % (design, residue))
                 if des_mutations_orient[residue]['from'] == '-' and residue > 0:
                     orient_offset += 1
                 elif des_mutations_orient[residue]['to'] == '-':  # all need to be removed from reference
                     for chain in oriented_pdb.chain_id_list:
                         oriented_pdb.delete_residue(chain, residue - orient_offset)
-                        # print('Design %s: Deleted residue %d from Oriented Input' % (design, residue - orient_offset))
+                        # logger.debug('Design %s: Deleted residue %d from Oriented Input' % (design, residue - orient_offset))
                 else:
                     for chain in oriented_pdb.chain_id_list:
                         oriented_pdb.mutate_to(chain, residue - orient_offset,
@@ -173,11 +186,11 @@ def design_recapitulation(design_file, pdb_dir, output_dir, oligomer=False):
             oriented_pdb_seq_final = oriented_pdb.atom_sequences[oriented_pdb.chain_id_list[0]]
             final_mutations = Ams.generate_mutations_from_seq(oriented_pdb_seq_final, asu.atom_sequences[chain_in_asu],
                                                               offset=False, blanks=True)
-            # print('ASU\t: %s' % asu.atom_sequences[chain_in_asu])
-            # print('Orient\t: %s' % oriented_pdb_seq_final)
+            logger.debug('ASU\t: %s' % asu.atom_sequences[chain_in_asu])
+            logger.debug('Orient\t: %s' % oriented_pdb_seq_final)
             if final_mutations != dict():
-                print('There is an error with indexing for Design %s, PDB %s. The index is %s' %
-                      (design, pdb, final_mutations))
+                logger.error('There is an error with indexing for Design %s, PDB %s. The index is %s' %
+                             (design, pdb, final_mutations))
                 break
 
             if not os.path.exists(os.path.join(output_dir, design, '%s_%s' % (i, sym))):
@@ -209,17 +222,17 @@ def design_recapitulation(design_file, pdb_dir, output_dir, oligomer=False):
             # {1_Sym: PDB1, 1_Sym2: PDB2, 'final_symmetry': I}
             sym_d = {'%s_%s' % (i, sym): pdb.lower() for i, (pdb, sym) in enumerate(design_file_input[design]['source_pdb'])}
             sym_d['final_symmetry'] = design_file_input[design]['final_sym']
-            # TODO remove _vflip
-            SDUtils.pickle_object(sym_d, name='%s_vflip_dock' % design, out_path=os.path.join(output_dir, design),
+            # 10/6/20 removed _vflip
+            SDUtils.pickle_object(sym_d, name='%s_dock' % design, out_path=os.path.join(output_dir, design),
                                   protocol=pickle_prot)
 
         # with open(os.path.join(output_dir, design, '%s_components.dock' % design), 'w') as f:
         #     f.write('\n'.join('%s %s' % (pdb.lower(), sym) for pdb, sym in design_file_input[design]['source_pdb']))
         #     f.write('\n%s %s' % ('final_symmetry', design_file_input[design]['final_sym']))
-    # TODO remove _vflip
+    # 10/6/20 removed _vflip
     if rmsd_comp_commands != dict():
         # {design: {'nanohedra_output': /path/to/directory, 'pdb1': /path/to/design_asu/pdb1_oligomer.pdb, 'pdb2': ...}}
-        SDUtils.pickle_object(rmsd_comp_commands, name='recap_rmsd_command_paths_vflip', out_path=output_dir,
+        SDUtils.pickle_object(rmsd_comp_commands, name='recap_rmsd_command_paths', out_path=output_dir,
                               protocol=pickle_prot)
 
     missing = []
@@ -229,9 +242,9 @@ def design_recapitulation(design_file, pdb_dir, output_dir, oligomer=False):
             chain_correspondence.pop(design)
     if missing != list():
         missing_str = map(str, missing)
-        print('Designs missing one of two chains:\n%s' % ', '.join(missing_str))
-    # TODO remove _vflip
-    SDUtils.pickle_object(chain_correspondence, 'asu_to_oriented_oligomer_chain_correspondance_vflip',
+        logger.critical('Designs missing one of two chains:\n%s' % ', '.join(missing_str))
+    # 10/6/20 removed _vflip
+    SDUtils.pickle_object(chain_correspondence, 'asu_to_oriented_oligomer_chain_correspondance',
                           out_path=output_dir, protocol=pickle_prot)
 
 
@@ -323,9 +336,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='\nTurn file(s) from a full PDB biological assembly into an ASU containing one copy of all entities'
                     ' in contact with the chain specified by chain')
+    parser.add_argument('-b', '--debug', action='store_true', help='Debug all steps to standard out?\nDefault=False')
     parser.add_argument('-d', '--directory', type=str, help='Directory where \'.pdb\' files to set up ASU extraction'
-                                                            'are located.\nDefault=CWD',
-                        default=os.getcwd())
+                                                            'are located.\nDefault=CWD',)
+    #                     default=os.getcwd())
     parser.add_argument('-f', '--file', type=str, help='File with list of pdb files of interest\nDefault=None',
                         default=None)
     parser.add_argument('-c', '--chain', type=str, help='What chain would you like to leave?\nDefault=A', default='A')
@@ -338,7 +352,17 @@ if __name__ == '__main__':
     parser.add_argument('-rot', '--flip', action='store_true', help='Whether to flip the orientation of a design')
 
     args = parser.parse_args()
-    logger = SDUtils.start_log()
+    # Start logging output
+    if args.debug:
+        logger = SDUtils.start_log(name=os.path.basename(__file__), level=1)
+        logger.debug('Debug mode. Verbose output')
+    else:
+        logger = SDUtils.start_log(name=os.path.basename(__file__), level=2)
+
+    logger.info('Starting %s with options:\n\t%s' %
+                (os.path.basename(__file__),
+                 '\n\t'.join([str(arg) + ':' + str(getattr(args, arg)) for arg in vars(args)])))
+
     # if args.directory and args.file:
     #     logger.error('Can only specify one of -d or -f not both. If you want to use a file list from a different '
     #                  'directory than the one you are in, you will have to navigate there!')
@@ -357,9 +381,9 @@ if __name__ == '__main__':
     # new_files = make_asu(file_list, args.chain, destination=args.output_destination)
     # logger.info('ASU files were written to:\n%s' % '\n'.join(new_files))
 
-    if not args.directory:
-        logger.error('No pdb directory specified. Please specify -d to collect the design PDBs')
-        exit()
+    # if not args.directory:
+    #     logger.error('No pdb directory specified. Please specify -d to collect the design PDBs')
+    #     exit()
     if not args.file:
         logger.error('No file specified. Please specify -f to collect the files')
         exit()
@@ -413,4 +437,4 @@ if __name__ == '__main__':
             run_rmsd_calc(design_d_names, args.design_map)
 
     else:
-        design_recapitulation(args.file, args.directory, args.out_path, args.oligomer_asu)
+        design_recapitulation(args.file, args.out_path, pdb_dir=args.directory, oligomer=args.oligomer_asu)
