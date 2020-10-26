678d2dbf5a6b0d6b14e36afcd72461dfa506cb4f
Kyle Meador <kylemeador@g.ucla.edu>
2020-10-26 00:36:00 -0700

    debug float

--- classes/PDB.py
+++ classes/PDB.py
@@ -612,9 +612,9 @@ class PDB:
                     surface_atoms.append(atom)
             return surface_atoms
 
-    def get_surface_resdiue_info(self, free_sasa_exe_path, probe_radius=2.2, sasa_thresh=0):
+    def get_surface_resdiue_info(self, free_sasa_ex_path, probe_radius=2.2, sasa_thresh=0):
         # only works for monomers or homo-complexes
-        proc = subprocess.Popen([free_sasa_exe_path, '--format=seq', '--probe-radius', probe_radius, self.filepath],
+        proc = subprocess.Popen([free_sasa_ex_path, '--format=seq', '--probe-radius', str(probe_radius), self.filepath],
                                 stdout=subprocess.PIPE)
         # proc = subprocess.Popen('%s --format=seq --probe-radius %s %s' % (free_sasa_exe_path, str(probe_radius),
         #                                                                   self.filepath), stdout=subprocess.PIPE,
