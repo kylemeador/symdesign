ade0b62d480ad52a110678792cbd0e58deacfd5e
Kyle Meador <kylemeador@g.ucla.edu>
2020-12-11 02:17:24 -0800

    modularization

--- interface_analysis/InterfaceSorting.py
+++ interface_analysis/InterfaceSorting.py
@@ -1,21 +1,48 @@
 import os
 
-import functions as fc
-from symdesign.SymDesignUtils import pickle, unpickle, get_all_pdb_file_paths
+import ParsePisa as pp
+from symdesign.SymDesignUtils import pickle_object, unpickle, get_all_pdb_file_paths, to_iterable
+
+# Globals
+pisa_type_extensions = {'multimers': '.xml', 'interfaces': '.xml', 'multimer': '.pdb', 'pisa': '.pkl'}
 
 
 # Todo PISA path
 def get_pisa_filepath(pdb_code, directory='/home/kmeador/yeates/fragment_database/all/pisa_files', file_type='pisa'):
     """Returns the PISA path that corresponds to the PDB code from the local PISA Database"""
-    pisa_type_extensions = {'multimers': '.xml', 'interfaces': '.xml', 'multimer': '.pdb', 'pisa': '.pkl'}
     if file_type in pisa_type_extensions:
+        pdb_code = pdb_code.upper()
         sub_dir = pdb_code[1:3].lower()
-        pisa_string = '_%s%s' % (file_type, pisa_type_extensions[file_type])
-        file_path = os.path.join(directory, sub_dir, ' %s%s' % (pdb_code, pisa_string))
-        if os.path.exists(file_path):
-            return file_path
+        root_path = os.path.join(directory, sub_dir)
+        specific_file = ' %s_%s%s' % (pdb_code, file_type, pisa_type_extensions[file_type])
+        if os.path.exists(os.path.join(root_path, specific_file)):
+            return os.path.join(root_path, specific_file)
+        else:  # attempt to make the file if not pickled
+            if file_type == 'pisa':
+                status = extract_pisa_files_and_pickle(root_path, pdb_code)
+                if status:
+                    return os.path.join(root_path, specific_file)
+                else:  # try to download required files
+                    dummy = True
+                    # TODO implement while loop for the download in the case file_type != 'pisa' or status = False
 
-    return False  # None
+    return None
+
+
+def extract_pisa_files_and_pickle(root, pdb_code):
+    """Take a set of pisa files acquired from pisa server and parse for saving as a complete pisa dictionary"""
+    individual_d = {}
+    try:
+        interface, chain = pp.parse_pisa_interfaces_xml(os.path.join(root, '%s_interfaces.xml' % pdb_code))
+        individual_d['interfaces'] = interface
+        individual_d['chains'] = chain
+        individual_d['multimers'] = pp.parse_pisa_multimers_xml(os.path.join(root, '%s_multimers.xml' % pdb_code))
+    except OSError:
+        return False
+
+    pickle_object(individual_d, '%s_pisa' % pdb_code, out_path=root)
+
+    return True
 
 
 def set_up_interface_dict(pdb_interface_codes):
@@ -29,134 +56,106 @@ def set_up_interface_dict(pdb_interface_codes):
         pdb_code = code.split('-')
         # ['1ABC', '3']
         if pdb_code[0] in int_dict:
-            int_dict[pdb_code[0]].add(int(pdb_code[1]))
+            int_dict[pdb_code[0].upper()].add(int(pdb_code[1]))
         else:
-            int_dict[pdb_code[0]] = set(int(pdb_code[1]))
+            int_dict[pdb_code[0].upper()] = set(int(pdb_code[1]))
 
     return int_dict
 
 
-def sort_interfaces_by_contact_type(int_d):
+def sort_interfaces_by_contact_type(pdb, pisa_d, interface_number_set, assembly_confirmed):
     """From a directory of interface pdbs with structure interface_pdbs/ab/1ABC-3.pdb
         grab the multimeric pisa xml file and search for all biological assemblies
 
     """
+    pisa_multimer = pisa_d['multimers']
+    pisa_interfaces = pisa_d['interfaces']
 
-    sorted_int_d, pdb_not_qsbio = {}, {}
-    for pdb in int_d:
-        if pdb in qsbio_confirmed:
-            pisa_path = get_pisa_filepath(pdb)
-            if 'pkl' in pisa_path:
-                pisa_d = unpickle(pisa_path)
-            else:
-                raise Exception('%s does not have the required file at %s' % (pdb, pisa_path))
-            multimer = pisa_d['multimers']
-            bio_ass = {}
-            if multimer[(1, 1)] != 'MONOMER':  # if the pdb is not a monomer
-                for set_complex in multimer:
-                    biological_assembly = multimer[set_complex]['pdb_BA']
-                    if biological_assembly != 0:  # if set_complex pair is a pdb_BiologicalAssembly
-                        if biological_assembly in bio_ass:  # is it in the BA dict?
-                            bio_ass[biological_assembly].append(set_complex)
-                        else:  # add to the BA dict
-                            bio_ass[biological_assembly] = [set_complex]
+    final_bio_int, final_xtal_int, final_unknown_int = set(), set(), set()
+    bio_ass = {}
+    if pisa_multimer[(1, 1)] != 'MONOMER':  # if the pdb is not a monomer
+        for set_complex in pisa_multimer:
+            biological_assembly = pisa_multimer[set_complex]['pdb_BA']
+            if biological_assembly != 0:  # if set_complex pair is a pdb_BiologicalAssembly
+                if biological_assembly in bio_ass:  # is it in the BA dict?
+                    bio_ass[biological_assembly].append(set_complex)
+                else:  # add to the BA dict
+                    bio_ass[biological_assembly] = [set_complex]
 
-                # Grab all unique interface numbers invoked in multimer entries if they are in QSBio
-                bio_int = []
-                for ba in bio_ass:
-                    # Todo make ba an int in qsbio_confrimed
-                    if str(ba) in qsbio_confirmed[pdb]:  # ensure biological assembly is QSBio confirmed assembly
-                        for set_complex in bio_ass[ba]:
-                            bio_int += multimer[set_complex]['interfaces'].keys()
-                # bio_int = list(set(bio_int))
+        # Grab all unique interface numbers invoked in multimer entries
+        # Use if they are not in QSBio
+        possible_bio_int = []
+        for ba in bio_ass:
+            for set_complex in bio_ass[ba]:
+                possible_bio_int += pisa_multimer[set_complex]['interfaces'].keys()
 
-                other_candidates = []
-                for candidate_int in bio_int:
-                    for rep in pisa_d['interfaces']['all_ids']:
-                        if candidate_int in pisa_d['interfaces']['all_ids'][rep]:
-                            other_candidates += pisa_d['interfaces']['all_ids'][rep]
-                bio_int = set(bio_int) | set(other_candidates)
+        other_candidates = []
+        for candidate_int in possible_bio_int:
+            for rep in pisa_interfaces['all_ids']:
+                if candidate_int in pisa_interfaces['all_ids'][rep]:
+                    other_candidates += pisa_interfaces['all_ids'][rep]
+        all_possible_bio_int = set(possible_bio_int) | set(other_candidates)
 
-                # finally insure that the biological_interface identified has an associated entry in the int_d
-                # final_bio_int = set(int_d[pdb]) & bio_int
-                final_bio_int = []
-                for interface in int_d[pdb]:
-                    # Todo make ba an int in int_dict and then take the intersection (&) of int_d[pdb] and bio_int
-                    if int(interface) in bio_int:
-                        final_bio_int.append(interface)
-            else:  # the pdb is a monomer
-                final_bio_int = []
-                # final_bio_int = {} Todo
-        else:  # the pdb is not a confirmed assembly
-            final_bio_int = []
-            # final_bio_int = {} Todo
-            pdb_not_qsbio[pdb] = []
-        int_d[pdb] = {'bio': set(final_bio_int), 'xtal': set(int_d[pdb]) - set(final_bio_int), 'unknown': set()}
-        # sorted_int_d[pdb] = {'bio': final_bio_int, 'xtal': set(int_d[pdb]) - final_bio_int, 'unknown': set()} Todo
-
-    # Filter out possible, not confirmed QSBio, biological interfaces from the xtal interface set for each PDB not in QSBio
-    for pdb in pdb_not_qsbio:
-        pisa_path = get_pisa_filepath(pdb)  # MOD DOUBLE
-        pisa_d = unpickle(pisa_path)  # DOUBLE
-        multimer = pisa_d['multimers']  # DOUBLE
-        bio_ass = {}  # DOUBLE
-        if multimer[(1, 1)] != 'MONOMER':  # DOUBLE
-            for set_complex in multimer:
-                biological_assembly = multimer[set_complex]['pdb_BA']
-                if biological_assembly != 0:  # if set_complex pair is a pdb_BiologicalAssembly
-                    if biological_assembly in bio_ass:  # is it in the BA dict?
-                        bio_ass[biological_assembly].append(set_complex)
-                    else:  # add to the BA dict
-                        bio_ass[biological_assembly] = [set_complex]
-
-            possible_bio_int = []
+        # finally insure that the biological_interface identified have confirmed assemblies
+        if assembly_confirmed != set():  # there are assemblies confirmed
+            # Only use ba if they are in QSBio
+            not_bio_int = []
             for ba in bio_ass:
-                pdb_not_qsbio[pdb].append(ba)  # MOD
-                for set_complex in bio_ass[ba]:
-                    possible_bio_int += multimer[set_complex]['interfaces'].keys()
-            possible_bio_int = list(set(possible_bio_int))
+                # Todo make ba an int in qsbio_confrimed
+                if str(ba) not in assembly_confirmed:  # ensure biological assembly is QSBio confirmed assembly
+                    for set_complex in bio_ass[ba]:
+                        not_bio_int += pisa_multimer[set_complex]['interfaces'].keys()
+                        # for interface in pisa_multimer[set_complex]['interfaces'].keys():
+                        #     all_possible_bio_int.remove(interface)
 
-            other_candidates = []
-            for candidate_int in possible_bio_int:
-                for rep in pisa_d['interfaces']['all_ids']:
-                    if candidate_int in pisa_d['interfaces']['all_ids'][rep]:
-                        other_candidates += pisa_d['interfaces']['all_ids'][rep]
-            possible_bio_int = set(possible_bio_int) | set(other_candidates)
-            # DOUBLE
+            bio_int = all_possible_bio_int - set(not_bio_int)
+            # take the intersection (&) of interface_number_set and bio_int
+            final_bio_int = interface_number_set & bio_int
+            final_xtal_int = interface_number_set - final_bio_int
+        else:  # the pdb does not have a confirmed assembly
+            # take the difference (-) of interface_number_set and bio_int
+            final_xtal_int = interface_number_set - all_possible_bio_int  # all interface identified, minus possible BA
+            final_unknown_int = interface_number_set - final_xtal_int
 
-            final_xtal_int = []
-            for interface in int_d[pdb]['xtal']:
-                if int(interface) not in possible_bio_int:
-                    final_xtal_int.append(interface)
-            final_xtal_int = set(final_xtal_int)
-            int_d[pdb]['unknown'] = int_d[pdb]['xtal'] - final_xtal_int
-            int_d[pdb]['xtal'] = final_xtal_int
+    return {pdb: {'bio': final_bio_int, 'xtal': final_xtal_int, 'unknown': final_unknown_int}}
 
-        final_bio_int, final_xtal_int, final_unknown_int = set(), set(), set()
-        if pdb in qsbio_confirmed:
-            # take the intersection (&) of int_d[pdb] and bio_int
-            final_bio_int = int_d[pdb] & bio_int
-            # final_bio_int = []
-            # for interface in int_d[pdb]:
-            #     if int(interface) in bio_int:
-            #         final_bio_int.append(interface)
-            final_xtal_int = int_d[pdb] - final_bio_int
-        else:
-            # final_xtal_int = []
-            # for interface in int_d[pdb]['xtal']:
-            # take the difference (-) of int_d[pdb] and bio_int
-            final_xtal_int = int_d[pdb] - bio_int
-            # for interface in int_d[pdb]:
-            #     if int(interface) not in bio_int:
-            #         final_xtal_int.append(interface)
-            # final_xtal_int = set(final_xtal_int)
-
-            # int_d[pdb]['unknown'] = int_d[pdb]['xtal'] - final_xtal_int
-            final_unknown_int = int_d[pdb] - final_xtal_int
-            # int_d[pdb]['xtal'] = final_xtal_int
-        sorted_int_d[pdb] = {'bio': final_bio_int, 'xtal': final_xtal_int, 'unknown': final_unknown_int}
-
-    return int_d, pdb_not_qsbio
+    # # Filter out possible, but not confirmed biological interfaces from the xtal interface set for each PDB
+    # for pdb in pdb_not_qsbio:
+    #     pisa_path = get_pisa_filepath(pdb)  # MOD DOUBLE
+    #     pisa_d = unpickle(pisa_path)  # DOUBLE
+    #     multimer = pisa_d['multimers']  # DOUBLE
+    #     bio_ass = {}  # DOUBLE
+    #     if multimer[(1, 1)] != 'MONOMER':  # DOUBLE
+    #         for set_complex in multimer:
+    #             biological_assembly = multimer[set_complex]['pdb_BA']
+    #             if biological_assembly != 0:  # if set_complex pair is a pdb_BiologicalAssembly
+    #                 if biological_assembly in bio_ass:  # is it in the BA dict?
+    #                     bio_ass[biological_assembly].append(set_complex)
+    #                 else:  # add to the BA dict
+    #                     bio_ass[biological_assembly] = [set_complex]
+    #
+    #         possible_bio_int = []
+    #         for ba in bio_ass:
+    #             pdb_not_qsbio[pdb].append(ba)  # MOD
+    #             for set_complex in bio_ass[ba]:
+    #                 possible_bio_int += multimer[set_complex]['interfaces'].keys()
+    #         possible_bio_int = list(set(possible_bio_int))
+    #
+    #         other_candidates = []
+    #         for candidate_int in possible_bio_int:
+    #             for rep in pisa_d['interfaces']['all_ids']:
+    #                 if candidate_int in pisa_d['interfaces']['all_ids'][rep]:
+    #                     other_candidates += pisa_d['interfaces']['all_ids'][rep]
+    #         possible_bio_int = set(possible_bio_int) | set(other_candidates)
+    #         # DOUBLE
+    #
+    #         final_xtal_int = []
+    #         for interface in int_d[pdb]['xtal']:
+    #             if int(interface) not in possible_bio_int:
+    #                 final_xtal_int.append(interface)
+    #         final_xtal_int = set(final_xtal_int)
+    #         int_d[pdb]['unknown'] = int_d[pdb]['xtal'] - final_xtal_int
+    #         int_d[pdb]['xtal'] = final_xtal_int
 
 
 if __name__ == '__main__':
@@ -179,16 +178,28 @@ if __name__ == '__main__':
     # print(clean_lines[:3])
     # print(interface_dict['2YV0'])
 
-    qsbio_file = '200121_pdb_lists/QSbio_GreaterThanHigh_Assemblies.pkl'
-    qsbio_confirmed = unpickle(qsbio_file)
-    interfaces_dir = 'all_interfaces'
+    current_interface_file_path = '/yeates1/kmeador/fragment_database/current_pdb_lists/'
+    interfaces_dir = '/yeates1/kmeador/fragment_database/all_interfaces'
+    qsbio_file = 'QSbio_GreaterThanHigh_Assemblies.pkl'
+    all_pdbs_of_interest = 'under3A_protein_no_multimodel_no_mmcif_no_bad_pisa.txt'
+
+    qsbio_confirmed_d = unpickle(os.path.join(current_interface_file_path, qsbio_file))
     all_pdb_paths = get_all_pdb_file_paths(interfaces_dir)
     pdb_interface_codes = list(file_ext_split[0] for file_ext_split in map(os.path.splitext, map(os.path.basename, all_pdb_paths)))
     interface_d = set_up_interface_dict(pdb_interface_codes)
 
-    final_int_dict, not_in_qsbio = sort_interfaces_by_contact_type(interface_d)
+    missing_pisa_paths = []
+    for pdb in interface_d:
+        pisa_path = get_pisa_filepath(pdb)
+        if pisa_path:
+            pisa_d = unpickle(pisa_path)
+        else:
+            missing_pisa_paths.append(pdb)
 
-    monomers_file = '200121_pdb_lists/QSbio_Monomers.csv'
+        final_int_dict = sort_interfaces_by_contact_type(interface_d, pisa_d, interface_d[pdb],
+                                                         set(qsbio_confirmed_d[pdb]))
+
+    monomers_file = os.path.join(current_interface_file_path, 'QSbio_Monomers.csv')
     with open(monomers_file, 'r') as f:
         all_lines = f.readlines()
         monomers = []
@@ -207,7 +218,7 @@ if __name__ == '__main__':
 
     proposed_qsbio_monomer_set = []
     for pdb in all_missing_biological:
-        if pdb in qsbio_confirmed:
+        if pdb in qsbio_confirmed_d:
             proposed_qsbio_monomer_set.append(pdb)
 
     confirmed_count = 0
@@ -218,7 +229,7 @@ if __name__ == '__main__':
     for entry in monomers:
         pdb = entry.split('_')[0].upper()
         starting_monomer_pdbs.append(pdb)
-        if pdb in qsbio_confirmed:
+        if pdb in qsbio_confirmed_d:
             confirmed_count += 1
         else:
             missing_from_confirmed.append(pdb)
@@ -231,7 +242,7 @@ if __name__ == '__main__':
     missing_monomers = set(final_monomers) - set(starting_monomer_pdbs)
     incorrect_assembly_assignment = list(set(proposed_qsbio_monomer_set) - set(final_monomers))
     print('Length of QSbio monomers (%d) should equal %d (all monomers confirmed in QSbio) and '
-          'maybe monomers missing from final (%d). If not, then those missing from final set (%d)'
+          'maybe monomers missing from final (%d). If not, then those missing from final (%d)'
           ' + final set monomers (%d) = %d should equal %d'
           % (len(monomers), confirmed_count, len(final_monomers), len(missing_from_final),
              len(final_monomers), len(missing_from_final) + len(final_monomers), len(monomers)))
@@ -267,10 +278,11 @@ if __name__ == '__main__':
             i = final_int_dict[entry]
         except KeyError:
             j += 1
-    print('There are actually', j,
-          'missing from the interface_dictionary that are in the monomers file. These should be outside of the interface selection criteria')
-    all_protein_file = '200121_pdb_lists/under3A_protein_no_multimodel_no_mmcif_no_bad_pisa.txt'
-    pdb_of_interest = fc.pdb_code_to_iterable(all_protein_file)
+    print('There are actually %d missing from the interface_dictionary that are in the monomers file. '
+          'These should be outside of the interface selection criteria')
+
+    all_protein_file = os.path.join(current_interface_file_path, all_pdbs_of_interest)
+    pdb_of_interest = to_iterable(all_protein_file)
     k = 0
     for entry in missing_from_final:
         try:
@@ -279,12 +291,12 @@ if __name__ == '__main__':
             k += 1
     print('Infact, there are', k, 'missing from proteins of interest in the first place')
 
-    final_file = '200121_pdb_lists/PDBInterfacesSorted.pkl'
-    pickle()
-    with open(final_file, 'wb') as f:
-        pickle.dump(final_int_dict, f, pickle.HIGHEST_PROTOCOL)
+    final_file = os.path.join(current_interface_file_path, 'PDBInterfacesSorted')  # .pkl'
+    # with open(final_file, 'wb') as f:
+    #     pickle.dump(final_int_dict, f, pickle.HIGHEST_PROTOCOL)
+    pickle_object(final_int_dict, final_file, out_path=current_interface_file_path)
 
-    pickle()
-    not_qsbio_file = '200121_pdb_lists/0204_not_qs_bio.pkl'
-    with open(not_qsbio_file, 'wb') as f:
-        pickle.dump(not_in_qsbio, f, pickle.HIGHEST_PROTOCOL)
+    # not_qsbio_file = '200121_pdb_lists/0204_not_qs_bio'  # .pkl'
+    # with open(not_qsbio_file, 'wb') as f:
+    #     pickle.dump(not_in_qsbio, f, pickle.HIGHEST_PROTOCOL)
+    # pickle_object(not_in_qsbio, not_qsbio_file, out_path=current_interface_file_path)
