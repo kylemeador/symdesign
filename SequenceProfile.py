940fd0b9f380f08cf9fd86028484436514366c10
kylemeador <kylemeador@g.ucla.edu>
2021-02-02 14:06:02 -0800

    fix evolutionary profile logic

--- SequenceProfile.py
+++ SequenceProfile.py
@@ -285,10 +285,13 @@ class SequenceProfile:
             self.log.info('Generating PSSM file for %s' % self.name)
             if profile_source == 'psiblast':
                 self.psiblast(out_path=out_path)
-                self.parse_psiblast_pssm()
             else:
                 self.hhblits(out_path=out_path)
-                self.parse_hhblits_pssm()
+
+        if profile_source == 'psiblast':
+            self.parse_psiblast_pssm()
+        else:
+            self.parse_hhblits_pssm()
 
     def psiblast(self, out_path=None, remote=False):
         """Generate an position specific scoring matrix using PSI-BLAST subprocess
