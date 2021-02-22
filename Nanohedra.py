bc2024a023bb73422bc43ece0b91f4a4fcd7d903
kylemeador <kylemeador@g.ucla.edu>
2021-02-22 10:23:02 -0800

    change frag_dock signature

--- Nanohedra.py
+++ Nanohedra.py
@@ -428,14 +428,13 @@ def main():
                 with open(master_log_filepath, "a+") as master_log_file:
                     master_log_file.write("Docking %s / %s \n" % (pdb1_filename, pdb2_filename))
 
-                nanohedra_dock(init_intfrag_cluster_rep_dict, ijk_intfrag_cluster_rep_dict, init_monofrag_cluster_rep_pdb_dict_1,
-                               init_monofrag_cluster_rep_pdb_dict_2, init_intfrag_cluster_info_dict,
-                               ijk_monofrag_cluster_rep_pdb_dict, ijk_intfrag_cluster_info_dict, master_outdir, pdb1_path,
-                               pdb2_path, set_mat1, set_mat2, ref_frame_tx_dof1, ref_frame_tx_dof2, is_internal_zshift1,
-                               is_internal_zshift2, result_design_sym, uc_spec_string, design_dim, expand_matrices, eul_lookup,
-                               init_max_z_val, subseq_max_z_val, degeneracy_matrices_1, degeneracy_matrices_2, rot_step_deg1,
-                               rot_range_deg_pdb1, rot_step_deg2, rot_range_deg_pdb2, output_exp_assembly, output_uc,
-                               output_surrounding_uc, min_matched, keep_time=timer)
+                nanohedra_dock(sym_entry, init_intfrag_cluster_rep_dict, ijk_intfrag_cluster_rep_dict,
+                               init_monofrag_cluster_rep_pdb_dict_1, init_monofrag_cluster_rep_pdb_dict_2,
+                               init_intfrag_cluster_info_dict, ijk_monofrag_cluster_rep_pdb_dict,
+                               ijk_intfrag_cluster_info_dict, master_outdir, pdb1_path, pdb2_path, init_max_z_val,
+                               subseq_max_z_val, rot_step_deg_pdb1=rot_step_deg1, rot_step_deg_pdb2=rot_step_deg2,
+                               output_exp_assembly=output_exp_assembly, output_uc=output_uc,
+                               output_surrounding_uc=output_surrounding_uc, min_matched=min_matched, keep_time=timer)
 
                 # master_log_file = open(master_log_filepath, "a+")
                 # master_log_file.write("COMPLETE ==> %s\n\n" % os.path.join(master_outdir, '%s_%s' %
