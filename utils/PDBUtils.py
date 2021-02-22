e9c11fc83bb7e7fa3c4b9c2d7620ab5d730f908a
kylemeador <kylemeador@g.ucla.edu>
2021-02-22 10:21:00 -0800

    remove entire atomic copying operation. only return transformed coords

--- utils/PDBUtils.py
+++ utils/PDBUtils.py
@@ -189,20 +189,21 @@ def get_interface_ghost_surf_frags(pdb1, pdb2, pdb1_ghost_frag_list, pdb2_surf_f
     # print "Length of Complete PDB1 Ghost Frag List: " + str(len(pdb1_ghost_frag_list)) + "\n"
     # print "Length of Complete PDB2 Surf Frag List: " + str(len(pdb2_surf_frag_list)) + "\n"
 
-    interface_ghost_frag_list = []
-    interface_ghost_frag_transformed_list = []
-
-    interface_ghost_frag_pdb_coords_list = []
-    interface_ghost_frag_pdb_coords_list_transformed = []
-    interface_ghost_frag_guide_coords_list_transformed = []
-
-    interface_surf_frag_list = []
-    interface_surf_frag_transformed_list = []
-
-    interface_surf_frag_pdb_coords_list = []
-    interface_surf_frag_pdb_coords_list_transformed = []
+    # interface_ghost_frag_list = []
+    # interface_ghost_frag_transformed_list = []
+    #
+    # interface_ghost_frag_pdb_coords_list = []
+    # interface_ghost_frag_pdb_coords_list_transformed = []
+    interface_ghost_frag_guide_coords_list = []
+    # interface_ghost_frag_guide_coords_list_transformed = []
+    #
+    # interface_surf_frag_list = []
+    # interface_surf_frag_transformed_list = []
+    #
+    # interface_surf_frag_pdb_coords_list = []
+    # interface_surf_frag_pdb_coords_list_transformed = []
     interface_surf_frag_guide_coords_list = []
-    interface_surf_frag_guide_coords_list_transformed = []
+    # interface_surf_frag_guide_coords_list_transformed = []
 
     pdb1_cb_coords, pdb1_cb_indices = pdb1.get_CB_coords(ReturnWithCBIndices=True, InclGlyCA=True)
     pdb2_cb_coords, pdb2_cb_indices = pdb2.get_CB_coords(ReturnWithCBIndices=True, InclGlyCA=True)
@@ -261,8 +262,9 @@ def get_interface_ghost_surf_frags(pdb1, pdb2, pdb1_ghost_frag_list, pdb2_surf_f
     # ghost_lookup_time_start = time.time()
     for ghost_frag in pdb1_ghost_frag_list:
         if ghost_frag.get_aligned_surf_frag_central_res_tup() in pdb1_central_resnum_chainid_unique_list:
-            interface_ghost_frag_list.append(ghost_frag)
-            interface_ghost_frag_pdb_coords_list.append(ghost_frag.get_pdb_coords())
+            # interface_ghost_frag_list.append(ghost_frag)
+            # interface_ghost_frag_pdb_coords_list.append(ghost_frag.get_pdb_coords())
+            interface_ghost_frag_guide_coords_list.append(ghost_frag.get_guide_coords())
     # ghost_lookup_time_end = time.time()
     # ghost_lookup_time = ghost_lookup_time_end - ghost_lookup_time_start
     # print "Ghost Lookup Time: " + str(ghost_lookup_time) + "\n"
@@ -270,8 +272,8 @@ def get_interface_ghost_surf_frags(pdb1, pdb2, pdb1_ghost_frag_list, pdb2_surf_f
     # surf_lookup_time_start = time.time()
     for surf_frag in pdb2_surf_frag_list:
         if surf_frag.get_central_res_tup() in pdb2_central_resnum_chainid_unique_list:
-            interface_surf_frag_list.append(surf_frag)
-            interface_surf_frag_pdb_coords_list.append(surf_frag.get_pdb_coords())
+            # interface_surf_frag_list.append(surf_frag)
+            # interface_surf_frag_pdb_coords_list.append(surf_frag.get_pdb_coords())
             interface_surf_frag_guide_coords_list.append(surf_frag.get_guide_coords())
     # surf_lookup_time_end = time.time()
     # surf_lookup_time = surf_lookup_time_end - surf_lookup_time_start
@@ -281,99 +283,103 @@ def get_interface_ghost_surf_frags(pdb1, pdb2, pdb1_ghost_frag_list, pdb2_surf_f
 
     # print "Interface Ghost Fragment PDB Coords List Length: " + str(np.vstack(interface_ghost_frag_pdb_coords_list).size) + "\n"
     # ghost_pdb_transform_time_start = time.time()
-    interface_ghost_frag_pdb_coords_list_transformed = rot_txint_set_txext_frag_coord_sets(
-        interface_ghost_frag_pdb_coords_list, rot_mat=rot_mat1, internal_tx_vec=internal_tx_vec1, set_mat=set_mat1,
+    # interface_ghost_frag_pdb_coords_list_transformed = rot_txint_set_txext_frag_coord_sets(
+    #     interface_ghost_frag_pdb_coords_list, rot_mat=rot_mat1, internal_tx_vec=internal_tx_vec1, set_mat=set_mat1,
+    #     ext_tx_vec=ext_tx_vec1)
+    interface_ghost_frag_guide_coords_list_transformed = rot_txint_set_txext_frag_coord_sets(
+        interface_ghost_frag_guide_coords_list, rot_mat=rot_mat1, internal_tx_vec=internal_tx_vec1, set_mat=set_mat1,
         ext_tx_vec=ext_tx_vec1)
-    # ghost_pdb_transform_time_end = time.time()
-    # ghost_pdb_transform_time = ghost_pdb_transform_time_end - ghost_pdb_transform_time_start
-    # print "Ghost PDB Transform Time: " + str(ghost_pdb_transform_time) + "\n"
-
-    # ghost_copy_time_start = time.time()
-    # print "Number of Interface Ghost Fragments: " + str(len(interface_ghost_frag_list)) + "\n"
-    for int_ghost_frag_index in range(len(interface_ghost_frag_list)):
-        int_ghost_frag = interface_ghost_frag_list[int_ghost_frag_index]
-        int_ghost_frag_pdb = int_ghost_frag.get_pdb()
-        int_ghost_frag_pdb_atoms = int_ghost_frag_pdb.get_atoms()
-
-        int_ghost_frag_transformed_pdb_coords = interface_ghost_frag_pdb_coords_list_transformed[int_ghost_frag_index]
-        int_ghost_frag_pdb_transformed = PDB()
-        int_ghost_frag_pdb_transformed_atoms = []
-        for atom_index in range(len(int_ghost_frag_pdb_atoms)):
-            atom = int_ghost_frag_pdb_atoms[atom_index]
-            x_transformed = int_ghost_frag_transformed_pdb_coords[atom_index][0]
-            y_transformed = int_ghost_frag_transformed_pdb_coords[atom_index][1]
-            z_transformed = int_ghost_frag_transformed_pdb_coords[atom_index][2]
-            atom_transformed = Atom(atom.get_number(), atom.get_type(), atom.get_alt_location(),
-                                    atom.get_residue_type(), atom.get_chain(), atom.get_residue_number(),
-                                    atom.get_code_for_insertion(), x_transformed, y_transformed, z_transformed,
-                                    atom.get_occ(), atom.get_temp_fact(), atom.get_element_symbol(),
-                                    atom.get_atom_charge())
-            int_ghost_frag_pdb_transformed_atoms.append(atom_transformed)
-        int_ghost_frag_pdb_transformed.set_all_atoms(int_ghost_frag_pdb_transformed_atoms)
-
-        int_ghost_frag_transformed = GhostFragment(int_ghost_frag_pdb_transformed, int_ghost_frag.get_i_frag_type(),
-                                                   int_ghost_frag.get_j_frag_type(), int_ghost_frag.get_k_frag_type(),
-                                                   int_ghost_frag.get_rmsd(),
-                                                   # int_ghost_frag.get_central_res_tup(),
-                                                   int_ghost_frag.get_aligned_surf_frag_central_res_tup(),
-                                                   # guide_atoms=int_ghost_frag_pdb_transformed_atoms[-3:],
-                                                   guide_coords=int_ghost_frag_transformed_pdb_coords[-3:])
-        #                                           pdb_coords=int_ghost_frag_transformed_pdb_coords)
-
-        interface_ghost_frag_transformed_list.append(int_ghost_frag_transformed)
-        interface_ghost_frag_guide_coords_list_transformed.append(int_ghost_frag_transformed.get_guide_coords())
-    # ghost_copy_time_end = time.time()
-    # ghost_copy_time = ghost_copy_time_end - ghost_copy_time_start
-    # print "Ghost Copy Time: " + str(ghost_copy_time) + "\n"
+    # # ghost_pdb_transform_time_end = time.time()
+    # # ghost_pdb_transform_time = ghost_pdb_transform_time_end - ghost_pdb_transform_time_start
+    # # print "Ghost PDB Transform Time: " + str(ghost_pdb_transform_time) + "\n"
+    #
+    # # ghost_copy_time_start = time.time()
+    # # print "Number of Interface Ghost Fragments: " + str(len(interface_ghost_frag_list)) + "\n"
+    # for int_ghost_frag_index in range(len(interface_ghost_frag_list)):
+    #     int_ghost_frag = interface_ghost_frag_list[int_ghost_frag_index]
+    #     int_ghost_frag_pdb = int_ghost_frag.get_pdb()
+    #     int_ghost_frag_pdb_atoms = int_ghost_frag_pdb.get_atoms()
+    #
+    #     int_ghost_frag_transformed_pdb_coords = interface_ghost_frag_pdb_coords_list_transformed[int_ghost_frag_index]
+    #     int_ghost_frag_pdb_transformed = PDB()
+    #     int_ghost_frag_pdb_transformed_atoms = []
+    #     for atom_index in range(len(int_ghost_frag_pdb_atoms)):
+    #         atom = int_ghost_frag_pdb_atoms[atom_index]
+    #         x_transformed = int_ghost_frag_transformed_pdb_coords[atom_index][0]
+    #         y_transformed = int_ghost_frag_transformed_pdb_coords[atom_index][1]
+    #         z_transformed = int_ghost_frag_transformed_pdb_coords[atom_index][2]
+    #         atom_transformed = Atom(atom.get_number(), atom.get_type(), atom.get_alt_location(),
+    #                                 atom.get_residue_type(), atom.get_chain(), atom.get_residue_number(),
+    #                                 atom.get_code_for_insertion(), x_transformed, y_transformed, z_transformed,
+    #                                 atom.get_occ(), atom.get_temp_fact(), atom.get_element_symbol(),
+    #                                 atom.get_atom_charge())
+    #         int_ghost_frag_pdb_transformed_atoms.append(atom_transformed)
+    #     int_ghost_frag_pdb_transformed.set_all_atoms(int_ghost_frag_pdb_transformed_atoms)
+    #
+    #     int_ghost_frag_transformed = GhostFragment(int_ghost_frag_pdb_transformed, int_ghost_frag.get_i_frag_type(),
+    #                                                int_ghost_frag.get_j_type(), int_ghost_frag.get_k_type(),
+    #                                                int_ghost_frag.get_rmsd(),
+    #                                                # int_ghost_frag.get_central_res_tup(),
+    #                                                int_ghost_frag.get_aligned_surf_frag_central_res_tup(),
+    #                                                # guide_atoms=int_ghost_frag_pdb_transformed_atoms[-3:],
+    #                                                guide_coords=int_ghost_frag_transformed_pdb_coords[-3:])
+    #     #                                           pdb_coords=int_ghost_frag_transformed_pdb_coords)
+    #
+    #     interface_ghost_frag_transformed_list.append(int_ghost_frag_transformed)
+    #     interface_ghost_frag_guide_coords_list_transformed.append(int_ghost_frag_transformed.get_guide_coords())
+    # # ghost_copy_time_end = time.time()
+    # # ghost_copy_time = ghost_copy_time_end - ghost_copy_time_start
+    # # print "Ghost Copy Time: " + str(ghost_copy_time) + "\n"
 
     # Rotate, Translate and Set Surface Fragment Guide Coordinates
     # surf_pdb_guidecoords_transform_time_start = time.time()
-    interface_surf_frag_pdb_coords_list_transformed = rot_txint_set_txext_frag_coord_sets(
-        interface_surf_frag_pdb_coords_list, rot_mat=rot_mat2, internal_tx_vec=internal_tx_vec2, set_mat=set_mat2,
-        ext_tx_vec=ext_tx_vec2)
+    # interface_surf_frag_pdb_coords_list_transformed = rot_txint_set_txext_frag_coord_sets(
+    #     interface_surf_frag_pdb_coords_list, rot_mat=rot_mat2, internal_tx_vec=internal_tx_vec2, set_mat=set_mat2,
+    #     ext_tx_vec=ext_tx_vec2)
     interface_surf_frag_guide_coords_list_transformed = rot_txint_set_txext_frag_coord_sets(
         interface_surf_frag_guide_coords_list, rot_mat=rot_mat2, internal_tx_vec=internal_tx_vec2, set_mat=set_mat2,
         ext_tx_vec=ext_tx_vec2)
-    # surf_pdb_guidecoords_transform_time_end = time.time()
-    # surf_pdb_guidecoords_transform_time = surf_pdb_guidecoords_transform_time_end - surf_pdb_guidecoords_transform_time_start
-    # print "Surf Transform Time: " + str(surf_pdb_guidecoords_transform_time) + "\n"
-
-    # print "Number of Interface Surface Fragments: " + str(len(interface_surf_frag_list)) + "\n"
-    # surf_copy_time_start = time.time()
-    for int_surf_frag_index in range(len(interface_surf_frag_list)):
-        int_surf_frag = interface_surf_frag_list[int_surf_frag_index]
-
-        int_surf_frag_pdb = int_surf_frag.get_pdb()
-        int_surf_frag_pdb_transformed = PDB()
-        int_surf_frag_transformed_pdb_coords = interface_surf_frag_pdb_coords_list_transformed[int_surf_frag_index]
-        int_surf_frag_transformed_pdb_atoms = []
-        int_surf_frag_pdb_atoms = int_surf_frag_pdb.get_atoms()
-        for atom_index in range(len(int_surf_frag_pdb_atoms)):
-            atom = int_surf_frag_pdb_atoms[atom_index]
-            x_transformed = int_surf_frag_transformed_pdb_coords[atom_index][0]
-            y_transformed = int_surf_frag_transformed_pdb_coords[atom_index][1]
-            z_transformed = int_surf_frag_transformed_pdb_coords[atom_index][2]
-            atom_transformed = Atom(atom.get_number(), atom.get_type(), atom.get_alt_location(),
-                                    atom.get_residue_type(), atom.get_chain(), atom.get_residue_number(),
-                                    atom.get_code_for_insertion(), x_transformed, y_transformed, z_transformed,
-                                    atom.get_occ(), atom.get_temp_fact(), atom.get_element_symbol(),
-                                    atom.get_atom_charge())
-            int_surf_frag_transformed_pdb_atoms.append(atom_transformed)
-        int_surf_frag_pdb_transformed.set_all_atoms(int_surf_frag_transformed_pdb_atoms)
-
-        int_surf_frag_guide_coords_transformed = interface_surf_frag_guide_coords_list_transformed[int_surf_frag_index]
-
-        int_surf_frag_transformed = MonoFragment.from_fragment(pdb=int_surf_frag_pdb_transformed,
-                                                               fragment_type=int_surf_frag.get_i_type(),
-                                                               guide_coords=int_surf_frag_guide_coords_transformed,
-                                                               central_res_num=int_surf_frag.get_central_res_num(),
-                                                               central_res_chain_id=int_surf_frag.get_central_res_chain_id())
-        #                                        ,pdb_coords=int_surf_frag_transformed_pdb_coords)
-
-        interface_surf_frag_transformed_list.append(int_surf_frag_transformed)
+    # # surf_pdb_guidecoords_transform_time_end = time.time()
+    # # surf_pdb_guidecoords_transform_time = surf_pdb_guidecoords_transform_time_end - surf_pdb_guidecoords_transform_time_start
+    # # print "Surf Transform Time: " + str(surf_pdb_guidecoords_transform_time) + "\n"
+    #
+    # # print "Number of Interface Surface Fragments: " + str(len(interface_surf_frag_list)) + "\n"
+    # # surf_copy_time_start = time.time()
+    # for int_surf_frag_index in range(len(interface_surf_frag_list)):
+    #     int_surf_frag = interface_surf_frag_list[int_surf_frag_index]
+    #
+    #     int_surf_frag_pdb = int_surf_frag.get_pdb()
+    #     int_surf_frag_pdb_transformed = PDB()
+    #     int_surf_frag_transformed_pdb_coords = interface_surf_frag_pdb_coords_list_transformed[int_surf_frag_index]
+    #     int_surf_frag_transformed_pdb_atoms = []
+    #     int_surf_frag_pdb_atoms = int_surf_frag_pdb.get_atoms()
+    #     for atom_index in range(len(int_surf_frag_pdb_atoms)):
+    #         atom = int_surf_frag_pdb_atoms[atom_index]
+    #         x_transformed = int_surf_frag_transformed_pdb_coords[atom_index][0]
+    #         y_transformed = int_surf_frag_transformed_pdb_coords[atom_index][1]
+    #         z_transformed = int_surf_frag_transformed_pdb_coords[atom_index][2]
+    #         atom_transformed = Atom(atom.get_number(), atom.get_type(), atom.get_alt_location(),
+    #                                 atom.get_residue_type(), atom.get_chain(), atom.get_residue_number(),
+    #                                 atom.get_code_for_insertion(), x_transformed, y_transformed, z_transformed,
+    #                                 atom.get_occ(), atom.get_temp_fact(), atom.get_element_symbol(),
+    #                                 atom.get_atom_charge())
+    #         int_surf_frag_transformed_pdb_atoms.append(atom_transformed)
+    #     int_surf_frag_pdb_transformed.set_all_atoms(int_surf_frag_transformed_pdb_atoms)
+    #
+    #     int_surf_frag_guide_coords_transformed = interface_surf_frag_guide_coords_list_transformed[int_surf_frag_index]
+    #
+    #     int_surf_frag_transformed = MonoFragment.from_fragment(pdb=int_surf_frag_pdb_transformed,
+    #                                                            fragment_type=int_surf_frag.get_i_type(),
+    #                                                            guide_coords=int_surf_frag_guide_coords_transformed,
+    #                                                            central_res_num=int_surf_frag.get_central_res_num(),
+    #                                                            central_res_chain_id=int_surf_frag.get_central_res_chain_id())
+    #
+    #     interface_surf_frag_transformed_list.append(int_surf_frag_transformed)
     # surf_copy_time_end = time.time()
     # surf_copy_time = surf_copy_time_end - surf_copy_time_start
     # print "Surf Copy Time: " + str(surf_copy_time) + "\n\n"
 
-    return interface_ghost_frag_transformed_list, interface_surf_frag_transformed_list, interface_ghost_frag_guide_coords_list_transformed, interface_surf_frag_guide_coords_list_transformed, len(
-        pdb1_central_resnum_chainid_unique_list), len(pdb2_central_resnum_chainid_unique_list)
+    # return  # interface_ghost_frag_transformed_list, interface_surf_frag_transformed_list, \
+    return np.array(interface_ghost_frag_guide_coords_list_transformed), \
+        np.array(interface_surf_frag_guide_coords_list_transformed), \
+        len(pdb1_central_resnum_chainid_unique_list), len(pdb2_central_resnum_chainid_unique_list)
