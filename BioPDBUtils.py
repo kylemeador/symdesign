01b9101adad9a132fd3cb55bcae475512b6873af
kylemeador <kylemeador@g.ucla.edu>
2021-03-02 14:14:21 -0800

    debug biopdb_aligned_chain

--- BioPDBUtils.py
+++ BioPDBUtils.py
@@ -25,6 +25,23 @@ def biopdb_aligned_chain(pdb_fixed, pdb_moving, chain_id_moving):
     return pdb_moving.return_transformed_copy(rotation=np.transpose(rot), translation=tr)
 
 
+# def biopdb_aligned_chain(pdb_fixed, chain_id_fixed, pdb_moving, chain_id_moving):
+def biopdb_align_atom_lists(atoms_fixed, atoms_moving):
+    # for atom in pdb_fixed.chain(chain_id_fixed).get_ca_atoms():
+    biopdb_atom_fixed = [BioPDBAtom(atom.type, (atom.x, atom.y, atom.z), atom.temp_fact, atom.occ, atom.alt_location,
+                                    " %s " % atom.type, atom.number, element=atom.element_symbol)
+                         for atom in atoms_fixed]
+    biopdb_atom_moving = [BioPDBAtom(atom.type, (atom.x, atom.y, atom.z), atom.temp_fact, atom.occ, atom.alt_location,
+                                     " %s " % atom.type, atom.number, element=atom.element_symbol)
+                          for atom in atoms_moving]
+    sup = Bio.PDB.Superimposer()
+    sup.set_atoms(biopdb_atom_fixed, biopdb_atom_moving)  # Todo remove Bio.PDB
+    rot, tr = sup.rotran
+    # return np.transpose(rot), tr
+    # transpose rotation matrix as Bio.PDB.Superimposer() returns correct matrix to rotate using np.matmul
+    return pdb_moving.return_transformed_copy(rotation=np.transpose(rot), translation=tr)
+
+
 def biopdb_superimposer(atoms_fixed, atoms_moving):
     """
 
