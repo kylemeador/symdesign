8f7a18c3f32d19a8b6acc60a253bce91b956e31c
kylemeador <kylemeador@g.ucla.edu>
2021-03-03 16:34:09 -0800

    test return_transformed_copy in biopdb_aligned_chain_old

--- BioPDBUtils.py
+++ BioPDBUtils.py
@@ -37,31 +37,33 @@ def biopdb_aligned_chain_old(pdb_fixed, chain_id_fixed, pdb_moving, chain_id_mov
     sup.set_atoms(biopdb_atom_fixed, biopdb_atom_moving)
     # no need to transpose rotation matrix as Bio.PDB.Superimposer() generates correct matrix to rotate using np.matmul
     rot, tr = sup.rotran[0], sup.rotran[1]
-
-    pdb_moving_coords_rot = np.matmul(pdb_moving.coords, rot)
-    pdb_moving_coords_rot_tx = pdb_moving_coords_rot + tr
-
-    # pdb_moving_copy = PDB()
-    # pdb_moving_copy.set_chain_id_list(pdb_moving.get_chain_id_list())
-    pdb_moving_copy_atom_list = []
-    # atom_count = 0
-    for idx, atom in enumerate(pdb_moving.atoms):
-        # x_transformed = pdb_moving_coords_rot_tx[atom_count][0]
-        # y_transformed = pdb_moving_coords_rot_tx[atom_count][1]
-        # z_transformed = pdb_moving_coords_rot_tx[atom_count][2]
-        atom_transformed = Atom(atom.index, atom.get_number(), atom.get_type(), atom.get_alt_location(),
-                                atom.get_residue_type(), atom.get_chain(),
-                                atom.get_residue_number(),
-                                atom.get_code_for_insertion(),  # x_transformed, y_transformed, z_transformed,
-                                atom.get_occ(), atom.get_temp_fact(), atom.get_element_symbol(),
-                                atom.get_atom_charge())  # , pdb_moving_coords_rot_tx[idx])
-        pdb_moving_copy_atom_list.append(atom_transformed)
-        # atom_count += 1
-
-    pdb_moving_copy = PDB.from_atoms(atoms=pdb_moving_copy_atom_list, coords=pdb_moving_coords_rot_tx, lazy=True,
-                                     log=None)
-
-    return pdb_moving_copy
+    # DOES this work though?
+    return pdb_moving.return_transformed_copy(rotation=np.transpose(rot), translation=tr)
+    # BELOW works GREAT
+    # pdb_moving_coords_rot = np.matmul(pdb_moving.coords, rot)
+    # pdb_moving_coords_rot_tx = pdb_moving_coords_rot + tr
+    #
+    # # pdb_moving_copy = PDB()
+    # # pdb_moving_copy.set_chain_id_list(pdb_moving.get_chain_id_list())
+    # pdb_moving_copy_atom_list = []
+    # # atom_count = 0
+    # for idx, atom in enumerate(pdb_moving.atoms):
+    #     # x_transformed = pdb_moving_coords_rot_tx[atom_count][0]
+    #     # y_transformed = pdb_moving_coords_rot_tx[atom_count][1]
+    #     # z_transformed = pdb_moving_coords_rot_tx[atom_count][2]
+    #     atom_transformed = Atom(atom.index, atom.get_number(), atom.get_type(), atom.get_alt_location(),
+    #                             atom.get_residue_type(), atom.get_chain(),
+    #                             atom.get_residue_number(),
+    #                             atom.get_code_for_insertion(),  # x_transformed, y_transformed, z_transformed,
+    #                             atom.get_occ(), atom.get_temp_fact(), atom.get_element_symbol(),
+    #                             atom.get_atom_charge())  # , pdb_moving_coords_rot_tx[idx])
+    #     pdb_moving_copy_atom_list.append(atom_transformed)
+    #     # atom_count += 1
+    #
+    # pdb_moving_copy = PDB.from_atoms(atoms=pdb_moving_copy_atom_list, coords=pdb_moving_coords_rot_tx, lazy=True,
+    #                                  log=None)
+    #
+    # return pdb_moving_copy
 
 
 # def biopdb_aligned_chain(pdb_fixed, chain_id_fixed, pdb_moving, chain_id_moving):
