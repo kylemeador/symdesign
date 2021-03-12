33cc4abdd23ad1c0e8fba08480ea642e931a348b
kylemeador <kylemeador@g.ucla.edu>
2021-03-11 23:00:43 -0800

    debug

--- classes/EulerLookup.py
+++ classes/EulerLookup.py
@@ -43,8 +43,12 @@ class EulerLookup:
         eulint2 = np.rint(e2_v * 180. / np.pi * 0.1 * 0.999999)
         eulint3 = (np.rint(e3_v * 180. / np.pi * 0.1 * 0.999999) + 36) % 36
 
-        # print(np.concatenate([eulint1, eulint2, eulint3]).reshape((len(v3_a), 3))[:5])
-        return np.concatenate([eulint1, eulint2, eulint3]).reshape((len(v3_a), 3))
+        eulint_array = np.concatenate([eulint1, eulint2, eulint3]).reshape((len(v3_a), 3))
+        print(eulint1[:5], eulint1.shape)
+        print(eulint2[:5], eulint2.shape)
+        print(eulint3[:5], eulint3.shape)
+        print(eulint_array[:5], eulint_array.shape)
+        return eulint_array
 
     @staticmethod
     def get_eulerint10_from_rot(rot):
@@ -126,7 +130,7 @@ class EulerLookup:
         # eulintarray2 = self.get_eulint_from_guides(guide_coords2.swapaxes(1, 2))  # swapaxes takes the inner transpose
         eulintarray1 = self.get_eulint_from_guides(guide_coords1)
         eulintarray2 = self.get_eulint_from_guides(guide_coords2)
-
+        print(eulintarray2[:5])
         # check lookup table
         try:
             euler_bool_l = []
