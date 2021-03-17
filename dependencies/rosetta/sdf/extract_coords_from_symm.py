a0903db9c94a41f9f7484ef45e953ab84ca278f8
kylemeador <kylemeador@g.ucla.edu>
2021-03-17 14:20:42 -0700

    remove nanohedra directory type constraint

---
+++ dependencies/rosetta/sdf/extract_coords_from_symm.py
@@ -0,0 +1,47 @@
+import sys
+import os
+import numpy as np
+
+
+
+def coords_to_pdb(coords):
+    chains = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '_', '+', ';', ':', ',', '<', '.', '>', '/', '?', '|', '[', '{', ']', '}', '`', '~']
+
+    atom_line = "ATOM  {:5d}  CA  GLY {:1s}{:4d}   {:8.3f}{:8.3f}{:8.3f}  1.00 20.00           C\n"
+    file_lines = []
+
+# make work for transformation of x vector by y vector. Throw Z away for the 0 dimension point groups
+    for i in range(len(coords)):
+        temp = []
+        for j in coords[i]:
+            temp.append(float(coords[i][j]))
+        atom_line.format(i, chains[i], i, temp[0], temp[1], temp[3])
+        file_lines.append(atom_line)
+
+    return file_lines
+
+
+if __name__ == '__main__':
+    file = sys.argv[1]
+    with open('file', 'r') as f:
+        new_coords = f.readlines()
+
+    coords = []
+    for i in new_coords:
+        temp = i.strip()
+        coords.append([temp.split()[-3], temp.split()[-2], temp.split()[-1]])
+    print(coords)
+
+    final_coords = np.zeros((len(coords), 3))
+    # final_coords = []
+    for i in range(len(coords)):
+        internal_coords = []
+        for j in range(len(3)):
+	    new = coords[i][j].strip()
+            new = new.strip('+')
+            coord = new.split(',')
+	    # internal_coords.append(coord)
+	    final_coords[i][j] = coord
+    # final_coords.append(internal_coords)
+    new_lines = coords_to_pdb(final_coords)
+
