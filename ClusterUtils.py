25d2d6916b95ae799a1f9af6c1ac8050e67f9dc9
kylemeador <kylemeador@g.ucla.edu>
2021-03-25 22:13:26 -0700

    implement clustering of transformations using DBSCAN to remove pose redundancy

--- ClusterUtils.py
+++ ClusterUtils.py
@@ -2,10 +2,17 @@ import os
 from itertools import combinations
 
 import numpy as np
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.decomposition import PCA
+# from scipy.spatial.distance import euclidean, pdist
+from sklearn.cluster import DBSCAN
+from sklearn.neighbors import NearestNeighbors
+import pandas as pd
 from Bio.PDB import PDBParser
 from Bio.PDB.Selection import unfold_entities
 
 import SymDesignUtils as SDUtils
+from utils.GeneralUtils import transform_coordinate_sets
 
 
 def pose_rmsd_mp(all_des_dirs, threads=1):
@@ -16,28 +23,29 @@ def pose_rmsd_mp(all_des_dirs, threads=1):
     Keyword Args:
         threads: Number of multiprocessing threads to run
     Returns:
-        (dict): {building_blocks: {pair1: {pair2: rmsd, ...}, ...}, ...}
+        (dict): {composition: {pair1: {pair2: rmsd, ...}, ...}, ...}
     """
     pose_map = {}
     pairs_to_process = []
     singlets = {}
-    for pair in combinations(all_des_dirs, 2):
-        # add all individual poses to a singles pool
-        singlets[pair[0].building_blocks] = pair[0]
-        if pair[0].building_blocks == pair[1].building_blocks:
-            singlets.pop(pair[0].building_blocks)
-            pairs_to_process.append(pair)
+    for pair1, pair2 in combinations(all_des_dirs, 2):
+        if pair1.composition == pair2.composition:
+            singlets.pop(pair1.composition, None)
+            pairs_to_process.append((pair1, pair2))
+        else:
+            # add all individual poses to a singles pool. pair2 is included in pair1, no need to add additional
+            singlets[pair1.composition] = pair1
     # find the rmsd between a pair of poses.  multiprocessing to increase throughput
     _results = SDUtils.mp_map(pose_pair_rmsd, pairs_to_process, threads=threads)
 
     # Make dictionary with all pairs
     for pair, pair_rmsd in zip(pairs_to_process, _results):
         protein_pair_path = os.path.basename(pair[0].building_blocks)
-        # protein_pair_path = pair[0].building_blocks
+        # protein_pair_path = pair[0].composition
         # pose_map[result[0]] = result[1]
         if protein_pair_path in pose_map:
-            # # {building_blocks: {(pair1, pair2): rmsd, ...}, ...}
-            # {building_blocks: {pair1: {pair2: rmsd, ...}, ...}, ...}
+            # # {composition: {(pair1, pair2): rmsd, ...}, ...}
+            # {composition: {pair1: {pair2: rmsd, ...}, ...}, ...}
             if str(pair[0]) in pose_map[protein_pair_path]:
                 pose_map[protein_pair_path][str(pair[0])][str(pair[1])] = pair_rmsd
                 if str(pair[1]) not in pose_map[protein_pair_path]:
@@ -67,7 +75,7 @@ def pose_pair_rmsd(pair):
     Returns:
         (float): RMSD value
     """
-    # protein_pair_path = pair[0].building_blocks
+    # protein_pair_path = pair[0].composition
     # Grab designed resides from the design_directory
     des_residue_list = [pose.info['design_residues'] for pose in pair]
 
@@ -94,8 +102,8 @@ def pose_pair_rmsd(pair):
 def pose_rmsd_s(all_des_dirs):
     pose_map = {}
     for pair in combinations(all_des_dirs, 2):
-        if pair[0].building_blocks == pair[1].building_blocks:
-            protein_pair_path = pair[0].building_blocks
+        if pair[0].composition == pair[1].composition:
+            protein_pair_path = pair[0].composition
             # Grab designed resides from the design_directory
             des_residue_list = [pose.info['des_residues'] for pose in pair]
             # could use the union as well...
@@ -134,24 +142,24 @@ def pose_rmsd_s(all_des_dirs):
             # if not pair_rmsd:
             #     continue
             if protein_pair_path in pose_map:
-                # {building_blocks: {(pair1, pair2): rmsd, ...}, ...}
+                # {composition: {(pair1, pair2): rmsd, ...}, ...}
                 if str(pair[0]) in pose_map[protein_pair_path]:
                     pose_map[protein_pair_path][str(pair[0])][str(pair[1])] = pair_rmsd
                     if str(pair[1]) not in pose_map[protein_pair_path]:
                         pose_map[protein_pair_path][str(pair[1])] = {str(pair[1]): 0.0}
                     # else:
                     #     print('\n' * 6 + 'NEVER ACCESSED' + '\n' * 6)
-                    #     pose_map[pair[0].building_blocks][str(pair[1])][str(pair[1])] = 0.0
+                    #     pose_map[pair[0].composition][str(pair[1])][str(pair[1])] = 0.0
                 # else:
                 #     print('\n' * 6 + 'ACCESSED' + '\n' * 6)
-                #     pose_map[pair[0].building_blocks][str(pair[0])] = {str(pair[1]): pair_rmsd}
-                #     pose_map[pair[0].building_blocks][str(pair[0])][str(pair[0])] = 0.0
-                # pose_map[pair[0].building_blocks][(str(pair[0]), str(pair[1]))] = pair_rmsd[2]
+                #     pose_map[pair[0].composition][str(pair[0])] = {str(pair[1]): pair_rmsd}
+                #     pose_map[pair[0].composition][str(pair[0])][str(pair[0])] = 0.0
+                # pose_map[pair[0].composition][(str(pair[0]), str(pair[1]))] = pair_rmsd[2]
             else:
                 pose_map[protein_pair_path] = {str(pair[0]): {str(pair[0]): 0.0}}
                 pose_map[protein_pair_path][str(pair[0])][str(pair[1])] = pair_rmsd
                 pose_map[protein_pair_path][str(pair[1])] = {str(pair[1]): 0.0}
-                # pose_map[pair[0].building_blocks] = {(str(pair[0]), str(pair[1])): pair_rmsd[2]}
+                # pose_map[pair[0].composition] = {(str(pair[0]), str(pair[1])): pair_rmsd[2]}
 
     return pose_map
 
@@ -160,13 +168,13 @@ def cluster_poses(pose_map):
     """Take a pose map calculated by pose_rmsd (_mp or _s) and cluster using DBSCAN algorithm
 
     Args:
-        pose_map (dict): {building_blocks: {pair1: {pair2: rmsd, ...}, ...}, ...}
+        pose_map (dict): {composition: {pair1: {pair2: rmsd, ...}, ...}, ...}
     Returns:
-        (dict): {building_block: {'poses clustered'}, ... }
+        (dict): {composition: {'poses clustered'}, ... }
     """
     pose_cluster_map = {}
-    for building_block in pose_map:
-        building_block_rmsd_df = pd.DataFrame(pose_map[building_block]).fillna(0.0)
+    for building_blocks in pose_map:
+        building_block_rmsd_df = pd.DataFrame(pose_map[building_blocks]).fillna(0.0)
 
         # PCA analysis of distances
         # pairwise_sequence_diff_mat = np.zeros((len(designs), len(designs)))
@@ -182,7 +190,7 @@ def cluster_poses(pose_map):
         # pca_distance_vector = pdist(building_block_rmsd_pc_np)
         # epsilon = pca_distance_vector.mean() * 0.5
 
-        # Compute the highest density cluster using DBSCAN algorithm
+        # Compute pose clusters using DBSCAN algorithm
         # logger.info('Finding pose clusters within RMSD of %f' % SDUtils.rmsd_threshold) # TODO
         dbscan = DBSCAN(eps=SDUtils.rmsd_threshold, min_samples=2, metric='precomputed')
         dbscan.fit(building_block_rmsd_matrix)
@@ -195,9 +203,11 @@ def cluster_poses(pose_map):
             cluster_ids.remove(-1)  # remove outlier label, will add all these later
         pose_indices = building_block_rmsd_df.index.to_list()
         cluster_members_map = {cluster_id: [pose_indices[n] for n, cluster in enumerate(dbscan.labels_)
-                                            if cluster == cluster_id] for cluster_id in cluster_ids}
-        # print(cluster_members_map)
-        # cluster_representative_map = {}
+                                            if cluster == cluster_id]
+                               for cluster_id in cluster_ids}
+
+        # Find the cluster representative
+        # use of dbscan.core_sample_indices_ returns all core_samples which is not a nearest neighbors mean index
         clustered_poses = {}
         for cluster in cluster_members_map:
             cluster_df = building_block_rmsd_df.loc[cluster_members_map[cluster], cluster_members_map[cluster]]
@@ -220,6 +230,118 @@ def cluster_poses(pose_map):
         # Add all outliers to the clustered poses as a representative
         clustered_poses.update({building_block_rmsd_df.index[idx]: building_block_rmsd_df.index[idx]
                                 for idx, cluster in enumerate(dbscan.labels_) if cluster == -1})
-        pose_cluster_map[building_block] = clustered_poses
+        pose_cluster_map[building_blocks] = clustered_poses
 
     return pose_cluster_map
+
+
+def cluster_transformations(transform1, transform2, distance=1.0):
+    """Cluster Pose conformations according to their specific transformation parameters to find Poses which occupy
+    essentially the same space
+
+    Args:
+        transform1 (dict[mapping[str, numpy.ndarray]]): A set of rotations/translations to be clustered
+        transform2 (dict[mapping[str, numpy.ndarray]]): A set of rotations/translations to be clustered
+    Keyword Args:
+        distance=1.0 (float): The distance to query neighbors in transformational space
+    Returns:
+        (tuple[numpy.ndarray(int), numpy.ndarray(int)]): Representative indices, cluster membership indices
+    """
+    # Todo tune DBSCAN distance (epsilon) to be reflective of the data, should be related to radius in NearestNeighbors
+    #  but smaller by some amount. Ideal amount would be the distance between two transformed guide coordinate sets of
+    #  a similar tx and a 3 degree step of rotation.
+    # make a blank set of guide coordinates for each incoming transformation
+    guide_coords = np.tile(np.array([[0., 0., 0.], [1., 0., 0.], [0., 1., 0.]]), (len(transform1), 1, 1))
+    # transforms = {'rotation': rotation_array, 'translation': translation_array,
+    #               'rotation2': rot2_array, 'translation2': tx2_array}
+    transformed_guide_coords1 = transform_coordinate_sets(guide_coords, **transform1)
+    transformed_guide_coords2 = transform_coordinate_sets(guide_coords, **transform2)
+    transformed_guide_coords = np.concatenate([transformed_guide_coords1, transformed_guide_coords2], axis=1)
+    # create a tree structure describing the distances of all transformed points relative to one another
+    transform_tree = NearestNeighbors(algorithm='ball_tree', radius=distance)
+    transform_tree.fit(transformed_guide_coords)
+    #                                sort_results returns only non-zero entries and provides the smallest distance first
+    distance_graph = transform_tree.radius_neighbors_graph(mode='distance')  # doesn't work? ->, sort_results=True)
+    #                                                      X=transformed_guide_coords is implied
+    # because this doesn't work to sort_results and pull out indices, I have to do another step 'radius_neighbors'
+    cluster = DBSCAN(eps=distance, min_samples=2, metric='precomputed').fit(distance_graph)  # , sample_weight=SOME WEIGHTS?
+    outliers = -1  # -1 are outliers in DBSCAN
+    labels = set(cluster.labels_) - {outliers}  # labels live here
+    tree_distances, tree_indices = transform_tree.radius_neighbors(mode='distance', sort_results=True)
+
+    # add all cluster means to the representative transformations
+    mean_cluster_dist = tree_distances.mean()
+    representative_transformation_indices = []
+    for label in labels:
+        representative_transformation_indices.append(mean_cluster_dist[np.where(cluster.labels_ == label)].argmin())
+    # add all outliers to representatives
+    representative_transformation_indices.append(np.where(cluster.labels_ == outliers))
+
+    return representative_transformation_indices, cluster.labels_
+
+
+def cluster_designs(composition_group):
+    """From a group of poses with matching protein composition, cluster the designs according to transformational
+    parameters
+
+    Args:
+        (iterable[DesignDirectory]): The group of DesignDirectory objects to pull transformation data from
+    Returns:
+        (dict[mapping[DesignDirectoryID, list[DesignDirectoryID]]): Cluster with the representative as the key and
+        matching poses as the values
+    """
+    # # First, identify the same compositions
+    # compositions = {}
+    # for design in design_directories:
+    #     if compositions.get(design.composition, None):
+    #         compositions[design.composition].append(design)
+    #     else:
+    #         compositions[design.composition] = [design]
+
+    # Next, identify the unique poses in each composition
+    # pose_cluster_map = {}
+    # for composition_group in compositions.values():
+    # format all transforms for the selected compositions
+    stacked_transforms = [design_directory.get_transformations() for design_directory in composition_group]
+    # transformations1 = [transform[1].values() for transform in stacked_transforms]
+    trans1_rot1, trans1_tx1, trans1_rot2, trans1_tx2 = zip(*[transform[1].values()
+                                                             for transform in stacked_transforms])
+    transformation1 = {'rotation': trans1_rot1, 'translation': trans1_tx1,
+                       'rotation2': trans1_rot2, 'translation2': trans1_tx2}
+
+    trans2_rot1, trans2_tx1, trans2_rot2, trans2_tx2 = zip(*[transform[2].values()
+                                                             for transform in stacked_transforms])
+    transformation2 = {'rotation': trans2_rot1, 'translation': trans2_tx1,
+                       'rotation2': trans2_rot2, 'translation2': trans2_tx2}
+
+    # rotation1, translation1, rotation2, translation2 = zip(*[design_directory.pose_transformation()
+    #                                                          for design_directory in design_directories])
+    # rotation1, translation1, rotation2, translation2 = stacked_transforms
+
+    # This section could be added to the Nanohedra docking routine
+    cluster_representative_indices, cluster_labels = cluster_transformations(transformation1, transformation2)
+    representative_labels = cluster_labels[cluster_representative_indices]
+
+    # pull out the pose-id from the input composition groups (DesignDirectory)
+    composition_map = {str(composition_group[rep_idx]): [str(composition_group[idx])
+                                                         for idx in np.flatnonzero(cluster_labels == rep_label).tolist()]
+                       for rep_idx, rep_label in zip(cluster_representative_indices, representative_labels)
+                       if rep_label != -1}  # don't add outliers (-1 labels) now
+    # add the outliers as separate occurrences
+    composition_map.update({str(composition_group[idx]): [] for idx in np.flatnonzero(cluster_labels == -1).tolist()})
+
+    return composition_map
+    # pose_cluster_map.update(composition_map)
+
+    # return pose_cluster_map
+
+
+def invert_cluster_map(cluster_map):
+    """Return an inverted cluster map where the cluster members map to the representative
+
+    Args:
+        cluster_map (dict[mapping[representative DesignDirectoryID, list[member DesignDirectoryID]])
+    Returns:
+        (dict[mapping[member DesignDirectoryID, representative DesignDirectoryID]])
+    """
+    return {pose: cluster_rep for cluster_rep, poses in cluster_map.items() for pose in poses}
