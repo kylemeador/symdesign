15ee64ae48c4b75d7838d7e2ea8a2aae6a744d4c
kylemeador <kylemeador@g.ucla.edu>
2021-04-10 21:28:50 -0700

    remove need to write file for SASA query

--- PDB.py
+++ PDB.py
@@ -1102,14 +1102,14 @@ class PDB(Structure):
         # SEQ A    8 LYS :    0.87
         # SEQ A    9 ASP :    1.30
         # SEQ A   10 PHE :   64.55
-        current_pdb_file = self.write(out_path='sasa_%s-%s-%d.pdb'
-                                               % (self.name, time.strftime('%y%m%d-%H%M%S'), int(random() * 1000)))
-        p = subprocess.Popen([free_sasa_exe_path, '--format=seq', '--probe-radius', str(probe_radius),
-                              current_pdb_file], stdout=subprocess.PIPE)  # stdin=subprocess.PIPE,
+        # temp_pdb_file = self.write(out_path='sasa_%s-%s-%d.pdb'
+        #                                        % (self.name, time.strftime('%y%m%d-%H%M%S'), int(random() * 1000)))
+        p = subprocess.Popen([free_sasa_exe_path, '--format=seq', '--probe-radius', str(probe_radius)],  # temp_pdb_file
+                             stdout=subprocess.PIPE, stdin=subprocess.PIPE)
         # Todo info can be passed as stdin. Replace read and write with PIPE
-        out, err = p.communicate()
-        os.system('rm %s' % current_pdb_file)
-        # out, err = p.communicate(input=str(self.atoms).encode('utf-8'))
+        # out, err = p.communicate()
+        # os.system('rm %s' % temp_pdb_file)
+        out, err = p.communicate(input=self.return_atom_string().encode('utf-8'))
 
         for line in out.decode('utf-8').split('\n'):
             # if line != "\n" and line != "" and not line.startswith("#"):
