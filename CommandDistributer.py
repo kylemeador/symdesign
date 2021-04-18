865abd6cb211468601da8140198c8de2d2dcc8da
kylemeador <kylemeador@g.ucla.edu>
2021-04-17 21:13:01 -0700

    remove flags_metrics for interface_design requirements

--- CommandDistributer.py
+++ CommandDistributer.py
@@ -33,26 +33,25 @@ extras_flags = {'default': [],
 script_cmd = [os.path.join(PUtils.rosetta, 'source/bin/rosetta_scripts.%s.linuxgccrelease' % PUtils.rosetta_extras),
               '-database', os.path.join(PUtils.rosetta, 'database')]
 rosetta_flags = extras_flags[PUtils.rosetta_extras] + \
-                ['-ex1', '-ex2', '-extrachi_cutoff 5', '-ignore_unrecognized_res',  # '-run:timer true',
-                 '-ignore_zero_occupancy false', '-overwrite', '-linmem_ig 10', '-out:file:scorefile_format json',
-                 '-output_only_asymmetric_unit true', '-no_chainend_ter true', '-write_seqres_records true',
-                 '-output_pose_energies_table false', '-output_pose_cache_data false',
-                 '-chemical:exclude_patches LowerDNA UpperDNA Cterm_amidation SpecialRotamer VirtualBB ShoveBB '
-                 'VirtualNTerm '
-                 'VirtualDNAPhosphate CTermConnect sc_orbitals pro_hydroxylated_case1 N_acetylated C_methylamidated '
-                 'cys_acetylated pro_hydroxylated_case2 ser_phosphorylated thr_phosphorylated tyr_phosphorylated '
-                 'lys_dimethylated lys_monomethylated lys_trimethylated lys_acetylated glu_carboxylated '
-                 'MethylatedProteinCterm tyr_diiodinated tyr_sulfated']
+    ['-ex1', '-ex2', '-extrachi_cutoff 5', '-ignore_unrecognized_res', '-ignore_zero_occupancy false',
+     '-overwrite',  # Todo remove
+     '-linmem_ig 10', '-out:file:scorefile_format json', '-output_only_asymmetric_unit true', '-no_chainend_ter true',
+     '-write_seqres_records true', '-output_pose_energies_table false', '-output_pose_cache_data false',
+     '-chemical:exclude_patches LowerDNA UpperDNA Cterm_amidation SpecialRotamer VirtualBB ShoveBB VirtualNTerm '
+     'VirtualDNAPhosphate CTermConnect sc_orbitals pro_hydroxylated_case1 N_acetylated C_methylamidated cys_acetylated'
+     'pro_hydroxylated_case2 ser_phosphorylated thr_phosphorylated tyr_phosphorylated tyr_diiodinated tyr_sulfated'
+     'lys_dimethylated lys_monomethylated lys_trimethylated lys_acetylated glu_carboxylated MethylatedProteinCterm',
+     '-mute all', '-unmute protocols.rosetta_scripts.ParsedProtocol']
 # 1 and 5 have the same flag options as both are relax
-flag_options = {PUtils.stage[1]: ['-constrain_relax_to_start_coords', '-use_input_sc', '-relax:ramp_constraints false',
-                                  '-no_optH false', '-relax:coord_constrain_sidechains', '-relax:coord_cst_stdev 0.5',
-                                  '-no_his_his_pairE', '-flip_HNQ', '-nblist_autoupdate true', '-no_nstruct_label true',
-                                  '-relax:bb_move false',  # '-out:suffix _' + PUtils.stage[1], '-o + PUtils.stage[1]],
-                                  '-mute all', '-unmute protocols.rosetta_scripts.ParsedProtocol'],
-                PUtils.stage[2]: ['-use_occurrence_data', '-out:suffix _' + PUtils.stage[2],  # '-o + PUtils.stage[2]],
-                                  '-mute all', '-unmute protocols.rosetta_scripts.ParsedProtocol'],  # -holes:dalphaball
-                PUtils.stage[3]: ['-no_nstruct_label true',  # '-out:suffix _' + PUtils.stage[2],
-                                  '-mute all', '-unmute protocols.rosetta_scripts.ParsedProtocol']}  # -out:pdb false
+flag_options = \
+    {PUtils.stage[1]: ['-constrain_relax_to_start_coords', '-use_input_sc', '-relax:ramp_constraints false',
+                       '-no_optH false', '-relax:coord_constrain_sidechains', '-relax:coord_cst_stdev 0.5',
+                       '-no_his_his_pairE', '-flip_HNQ', '-nblist_autoupdate true', '-no_nstruct_label true',
+                       '-relax:bb_move false'],  # '-out:suffix _' + PUtils.stage[1], '-o + PUtils.stage[1]],
+     PUtils.stage[2]: ['-use_occurrence_data', '-out:suffix _%s' % PUtils.stage[2]]  # '-holes:dalphaball']
+     }
+#                 PUtils.stage[3]: [  # '-no_nstruct_label true',  # '-out:suffix _' + PUtils.stage[2],
+#                                   '-mute all', '-unmute protocols.rosetta_scripts.ParsedProtocol']}  # -out:pdb false
 
 # Those jobs having a scale of 2 utilize two threads. Therefore two commands are selected from a supplied commands list
 # and are launched inside a python environment once the SLURM controller starts a SBATCH array job
