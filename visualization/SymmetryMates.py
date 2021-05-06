f42471349f9d7467306cdafd4afd7ebc365852ba
kylemeador <kylemeador@g.ucla.edu>
2021-05-06 15:53:35 -0700

    updated with function to save the expanded model

--- visualization/SymmetryMates.py
+++ visualization/SymmetryMates.py
@@ -1,5 +1,5 @@
 import os
-from pymol import cmd
+from pymol import cmd, stored
 
 from Pose import Pose
 from SymDesignUtils import possible_symmetries
@@ -41,4 +41,38 @@ def expand(name=None, symmetry=None):
     generate_symmetry_mates_pymol(name, expand_matrices)
 
 
+def save_group(group='all', one_file=True, out_dir=os.getcwd()):
+    """Save all files inside a group to either one file or a list of files.
+    Assumes the groups were generated using 'expand'
+
+    Keyword Args:
+        group='all' (str): name of the group to save
+        one_file=True (bool): Saves each protein in the group to one file, if False, then saves all members individually
+        out_dir=os.get_cwd() (str): The directory location to save the files to
+
+    Returns:
+        (None)
+    """
+    if group == 'all':
+        # remove appended symmetry mate number '_#' from the group instances and take the set of structures
+        expanded_group = set('_'.join(name.split('_')[:-1]) for name in cmd.get_names('group_objects', enabled_only=1))
+        groups = ['%s_expanded' % group for group in expanded_group]
+    elif cmd.get_type(group) == 'object:group':
+        groups = [group]
+    else:
+        print('Error: please provide a group name to save.')
+        return None
+
+    if one_file:
+        for group in groups:
+            cmd.save(os.path.join(out_dir, '%s.pdb' % group), group)
+    else:
+        for group in groups:
+            stored.models = set()
+            cmd.iterate(group, 'stored.models.add(model)')
+            for model in stored.models:
+                cmd.save(os.path.join(out_dir, '%s.pdb' % model), model)
+
+
 cmd.extend('expand', expand)
+cmd.extend('save_group', save_group)
