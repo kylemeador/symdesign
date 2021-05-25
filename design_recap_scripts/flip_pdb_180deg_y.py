3d6a3441f7ba1b07205a0032ae69143b27f267c8
kylemeador <kylemeador@g.ucla.edu>
2021-05-25 15:24:20 -0700

    reduced footprint

--- design_recap_scripts/flip_pdb_180deg_y.py
+++ design_recap_scripts/flip_pdb_180deg_y.py
@@ -2,19 +2,10 @@ import os
 import sys
 
 from PDB import PDB
-# from utils.PDBUtils import rot_txint_set_txext_pdb
-
-# September 29th 2020
-# Joshua Laniado
+from classes.SymEntry import flip_y_matrix
 
 pdb_filepath = sys.argv[1]
-flipped_pdb_out_path = os.path.splitext(pdb_filepath)[0] + "_flipped_180y.pdb"
 
-rot_mat1 = [[-1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, -1.0]]  # rot 180y
-
-pdb = PDB()
-pdb.readfile(pdb_filepath)
-
-# flipped_pdb = rot_txint_set_txext_pdb(pdb, rot_mat=rot_mat1)
-flipped_pdb = pdb.return_transformed_copy(rotation=rot_mat1)
-flipped_pdb.write(flipped_pdb_out_path)
+pdb = PDB.from_file(pdb_filepath)
+flipped_pdb = pdb.return_transformed_copy(rotation=flip_y_matrix)
+flipped_pdb.write('%s_flipped_180y.pdb' % os.path.splitext(pdb_filepath)[0])
