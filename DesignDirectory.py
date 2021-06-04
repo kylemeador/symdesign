268bccbd277ae2663b3d62af96b6e946f28c9789
kylemeador <kylemeador@g.ucla.edu>
2021-06-04 15:05:05 -0700

    implement constant pickled memory error patch

--- DesignDirectory.py
+++ DesignDirectory.py
@@ -577,17 +577,19 @@ class DesignDirectory:  # Todo move PDB coordinate information to Pose. Only use
             self.pose_file = os.path.join(self.path, PUtils.pose_file)
             self.frag_file = os.path.join(self.frags, PUtils.frag_text_file)
             if os.path.exists(self.serialized_info):  # Pose has already been processed, gather state data
-                print('Found pickled file with size %d' % os.stat(self.serialized_info).st_size)
                 self.info = unpickle(self.serialized_info)
+                if os.stat(self.serialized_info).st_size > 10000:
+                    print('Found pickled file with huge size %d. fragmentdatabase being removed'
+                          % os.stat(self.serialized_info).st_size)
+                    self.info['fragment_database'] = \
+                        getattr(self.info.get('fragment_database'), 'source', 'biological_interfaces')
+                    self.pickle_info()  # save this info so that we don't have this issue again!
                 self._info = self.info.copy()  # create a copy of the state upon initialization
                 # if self.info.get('nanohedra'):
                 self.transform_d = self.info.get('pose_transformation', dict())
                 self.oligomer_names = self.info.get('oligomer_names', list())
                 self.entity_names = self.info.get('entity_names', list())
-                # self.info['fragment_database'] = \
-                #     getattr(self.info.get('fragment_database'), 'source', 'biological_interfaces')
                 # self._info = self.info.copy()  # create a copy of the state upon initialization
-                # self.pickle_info()  # save this info on the first copy so that we don't have to construct again
                 # self.pre_refine = self.info.get('pre_refine', True)  # Todo remove after T33
                 self.fragment_observations = self.info.get('fragments', None)  # None signifies query wasn't attempted
                 self.interface_residue_ids = self.info.get('interface_residues', {})
