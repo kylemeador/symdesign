8810bf61b4c1fa4ca07b8f613c99dabbbdc05636
kylemeador <kylemeador@g.ucla.edu>
2021-06-05 11:42:48 -0700

    add hbnet_design_profile to stage dict

--- PathUtils.py
+++ PathUtils.py
@@ -25,7 +25,7 @@ rosetta = os.environ.get('ROSETTA')
 nstruct = 20  # Todo back to 50?
 stage = {1: 'refine', 2: 'design', 3: 'metrics', 4: 'analysis', 5: 'consensus',
          6: 'rmsd_calculation', 7: 'all_to_all', 8: 'rmsd_clustering', 9: 'rmsd_to_cluster', 10: 'rmsd',
-         11: 'all_to_cluster', 12: 'scout'}
+         11: 'all_to_cluster', 12: 'scout', 13: 'hbnet_design_profile'}
 stage_f = {stage[1]: {'path': '*_refine.pdb', 'len': 1}, stage[2]: {'path': '*_design_*.pdb', 'len': nstruct},
            stage[3]: {'path': '', 'len': None}, stage[4]: {'path': '', 'len': None},
            stage[5]: {'path': '*_consensus.pdb', 'len': 1}, 'nanohedra': {'path': '', 'len': None},
