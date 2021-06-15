0def5678adea99eba5a0c3b9c399a90adf5efedb
kylemeador <kylemeador@g.ucla.edu>
2021-06-14 17:59:51 -0700

    correct formatting of arrays to script

--- dependencies/bin/sort_hbnet_silent_file_results.sh
+++ dependencies/bin/sort_hbnet_silent_file_results.sh
@@ -21,9 +21,10 @@ hbnet_res_count=$(awk -v field=R_core_design_residue_count 'NR==1{for(i=1 ; i<=N
 
 # sort -k"$khbnet_sc"n -k"$khbnet_density"n $hbnet_scores	# nr sorts each key (k) numerically (n) in descending (r) order->Higher at the top	# test
 # sort the scores by the respective column numbers identified, then extract the decoy name
-sc_array=($(tail -n +2 $hbnet_scores | sort -k"$hbnet_sc"nr | head -n $(($2 * 3)) | awk -v field=$decoy '{print $field}'))
-density_array=($(tail -n +2 $hbnet_scores | sort -k"$hbnet_density"n | head -n $(($2 * 3)) | awk -v field=$decoy '{print $field}'))
-residue_count_array=($(tail -n +2 $hbnet_scores | sort -k"$hbnet_res_count"nr | awk -v field=$decoy '{print $field}'))
+# only sorts the first example
+sc_array=($(tail -n +2 $hbnet_scores | sort -u -k"$decoy"r | sort -k"$hbnet_sc"nr | head -n $(($2 * 3)) | awk -v field=$decoy '{print $field}'))
+density_array=($(tail -n +2 $hbnet_scores | sort -u -k"$decoy"r | sort -k"$hbnet_density"n | head -n $(($2 * 3)) | awk -v field=$decoy '{print $field}'))
+residue_count_array=($(tail -n +2 $hbnet_scores | sort -u -k"$decoy"r | sort -k"$hbnet_res_count"nr | awk -v field=$decoy '{print $field}'))
 
 
 # sort the elements in both density and sc (shape complementarity) array by the sc order adding to "overlap"
@@ -57,17 +58,37 @@ if [ -f $hbnet_tags_to_grab ]; then
   rm $hbnet_tags_to_grab
 fi
 final_silent_file=$(dirname -- "$1")/hbnet_selected.o
-if [ -f $final_silent_file ]; then
-  rm $final_silent_file
-fi
+#if [ -f $final_silent_file ]; then
+#  rm $final_silent_file
+#fi
 grep "^SEQUENCE" --max-count=1 $1 > $final_silent_file
 grep "^SCORE" --max-count=1 $1 >> $final_silent_file
+echo "REMARK BINARY SILENTFILE" >> $final_silent_file
 
 # finally, create the decoys of interest adding each decoy until the specified number provided in $2 is reached
 for ((i=0 ; i<$2 ; i++)); do
   echo ${final_overlap[$i]} >> $hbnet_tags_to_grab
   structure_line_numbers=($(grep "${final_overlap[$i]}" -n $1 | cut --fields=1 --delimiter=:))
-  head -n ${structure_line_numbers[-1]} $1 | tail -$((${structure_line_numbers[-1]} - ${structure_line_numbers[0]} + 1)) >> $final_silent_file
+  duplicate_structure=0
+  last_array_num=$((${structure_line_numbers[0]} - 1))  # always satisfies initial condition
+  for ((array_num=0 ; array_num<${#structure_line_numbers[@]} ; array_num++)); do
+    # is the next line way different than the last and 100 away from line start?
+    # assumes that the silent file has at least 100 lines present thus allows 99 REMARK lines to be read (current investigation shows >300 lines usually)
+    if [[ $((${structure_line_numbers[$array_num]} - 1)) != $last_array_num && $array_num > 100 ]]; then
+      duplicate_structure=$last_array_num  #${structure_line_numbers[$array_num]}
+      break
+    fi
+    last_array_num=${structure_line_numbers[$array_num]}
+  done
+  if [ $duplicate_structure == 0 ]; then  # not a duplicate structure
+    head -n ${structure_line_numbers[-1]} $1 | tail -$((${structure_line_numbers[-1]} - ${structure_line_numbers[0]} + 1)) >> $final_silent_file
+    else  # only take the first example
+      echo "Found a duplicate structure starting at line $duplicate_structure taking only the first occurrence"
+      head -n $duplicate_structure $1 | tail -$(($duplicate_structure - ${structure_line_numbers[0]} + 1)) >> $final_silent_file  # | awk -v decoy=${final_overlap[$i]}_0001 '{print $1, decoy}' >> $final_silent_file
+#      this won't work when the duplicate is back to back...
+#      head -n ${structure_line_numbers[-1]} $1 | tail -$((${structure_line_numbers[-1]} - $duplicate_structure + 1)) >> $final_silent_file
+  fi
+
 done
 
 # cat $hbnet_tags_to_grab																														# test
