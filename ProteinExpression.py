f972e2f548addc857fe39dcb804a15c304ecfe9c
Kyle Meador <kylemeador@g.ucla.edu>
2021-06-21 16:35:26 -0500

    debug

--- ProteinExpression.py
+++ ProteinExpression.py
@@ -380,10 +380,14 @@ def optimize_protein_sequence(sequence, species='e_coli'):
     problem.optimize()
 
     # PRINT SUMMARIES TO CHECK THAT CONSTRAINTS PASS
-    print(problem.constraints_text_summary())
-    print(problem.objectives_text_summary())
+    # print(problem.constraints_text_summary())
+    # print(problem.objectives_text_summary())
 
     # GET THE FINAL SEQUENCE (AS STRING OR ANNOTATED BIOPYTHON RECORDS)
     final_sequence = problem.sequence  # string
+    print('1', final_sequence)
+    final_sequence = problem.sequence.seq  # string
+    print('2', final_sequence)
+
     # final_record = problem.to_record(with_sequence_edits=True)
     return final_sequence
