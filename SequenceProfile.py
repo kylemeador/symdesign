1dca5e9e698fb0c55dbf8ec261ac1cca4cd24e5a
kylemeador <kylemeador@g.ucla.edu>
2021-07-08 12:37:27 -0700

    remove debug

--- SequenceProfile.py
+++ SequenceProfile.py
@@ -496,11 +496,9 @@ class SequenceProfile:
         msa_np = np.array([list(record) for record in self.msa.alignment], np.character)
         aligned_hci_np = np.zeros((self.msa.number_of_sequences, self.msa.length))
         evolutionary_collapse_np = aligned_hci_np.copy()
-        print('alignment', self.msa.alignment[:5, :])
+        # print('alignment', self.msa.alignment[:5, :])
         for idx, record in enumerate(self.msa.alignment):
-            print(str(record.seq))
             non_gapped_sequence = str(record.seq).replace('-', '')
-            print(non_gapped_sequence)
             evolutionary_collapse_np[idx, :len(non_gapped_sequence)] = hydrophobic_collapse_index(non_gapped_sequence)
 
         print('all collapse', evolutionary_collapse_np[:5, :])
