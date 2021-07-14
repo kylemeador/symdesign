d71ce6d0045c7de549e879af881b8a6293c03a78
kylemeador <kylemeador@g.ucla.edu>
2021-07-14 15:00:13 -0700

    brief removal of bmdca

--- interface_analysis/Database.py
+++ interface_analysis/Database.py
@@ -11,7 +11,7 @@ from SequenceProfile import parse_hhblits_pssm, MultipleSequenceAlignment, read_
 from Structure import parse_stride
 from SymDesignUtils import DesignError, unpickle, get_all_base_root_paths, start_log, dictionary_lookup
 from utils.MysqlPython import Mysql
-import dependencies.bmdca as bmdca
+# import dependencies.bmdca as bmdca
 
 
 # Globals
@@ -34,10 +34,10 @@ class Database:  # Todo ensure that the single object is completely loaded befor
         self.sequences = DataStore(location=sequences, extension='.fasta', sql=sql, log=log)
         self.alignments = DataStore(location=hhblits_profiles, extension='.sto', sql=sql, log=log)
         self.hhblits_profiles = DataStore(location=hhblits_profiles, extension='.hmm', sql=sql, log=log)
-        self.bmdca_fields = \
-            DataStore(location=hhblits_profiles, extension='_bmDCA%sparameters_h_final.bin' % os.sep, sql=sql, log=log)
-        self.bmdca_couplings = \
-            DataStore(location=hhblits_profiles, extension='_bmDCA%sparameters_J_final.bin' % os.sep, sql=sql, log=log)
+        # self.bmdca_fields = \
+        #     DataStore(location=hhblits_profiles, extension='_bmDCA%sparameters_h_final.bin' % os.sep, sql=sql, log=log)
+        # self.bmdca_couplings = \
+        #     DataStore(location=hhblits_profiles, extension='_bmDCA%sparameters_J_final.bin' % os.sep, sql=sql, log=log)
 
     def load_all_data(self):
         """For every resource, acquire all existing data in memory"""
