7e87258cd979e8ce8569b4170fbcf97e3d4b1d3f
Kyle Meador <kylemeador@g.ucla.edu>
2021-08-28 18:40:17 -0700

    debug melting temperature constraint

--- ProteinExpression.py
+++ ProteinExpression.py
@@ -370,7 +370,8 @@ def optimize_protein_sequence(sequence, species='e_coli'):
         (str): The input sequence optimized to nucleotides for expression considerations
     """
     seq_length = len(sequence)
-    problem = DnaOptimizationProblem(sequence=reverse_translate(sequence),
+    problem = DnaOptimizationProblem(sequence=reverse_translate(sequence), max_random_iters=20000,
+                                     objectives=[CodonOptimize(species=species)], logger=None,
                                      constraints=[EnforceGCContent(mini=0.25, maxi=0.65),  # twist required
                                                   EnforceGCContent(mini=0.35, maxi=0.65, window=50),  # twist required
                                                   AvoidHairpins(stem_size=20, hairpin_window=48),  # efficient translate
@@ -384,10 +385,10 @@ def optimize_protein_sequence(sequence, species='e_coli'):
                                                   AvoidRareCodons(0.08, species=species),
                                                   EnforceTranslation(),
                                                   EnforceMeltingTemperature(mini=10, maxi=62, location=(1, seq_length)),
-                                                  ], objectives=[CodonOptimize(species=species)], logger=None)
+                                                  ])
 
     # Solve constraints and solve with regards to the objective
-    problem.max_random_iters = 20000
+    # problem.max_random_iters = 20000
     problem.resolve_constraints()
     problem.optimize()
 
