1de406a6d7d60366c292d8924ebf52e8dcfdd1f7
kylemeador <kylemeador@g.ucla.edu>
2021-09-06 21:20:52 -0700

    debug uniprot_id

--- Structure.py
+++ Structure.py
@@ -2304,7 +2304,7 @@ class Entity(Chain, SequenceProfile):
         except AttributeError:
             try:
                 chain_api_data = self.api_entry[next(iter(self.api_entry))]
-                print('ERROR Retrieving UNP ID for %s' % self.name)
+                print('ERROR Retrieving UNP ID for %s\nAPI DATA:\n%s' % (self.name, chain_api_data))
                 if chain_api_data.get('db', None) == 'UNP':
                     self._uniprot_id = chain_api_data.get('accession', None)
                 return self._uniprot_id
