5367b1be8a798ae59d696640e2e5d9c05a2a98d6
kylemeador <kylemeador@g.ucla.edu>
2021-09-08 12:53:24 -0700

    improve function parsing of ialign

--- ClusterUtils.py
+++ ClusterUtils.py
@@ -188,10 +188,18 @@ def ialign(pdb_file1, pdb_file2, chain1=None, chain2=None, out_path=os.path.join
     ialign_p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     ialign_out, ialign_err = ialign_p.communicate()
     # out, err = p.communicate(input=self.return_atom_string().encode('utf-8'))
+    print(ialign_out.decode())
     grep_p = subprocess.Popen(['grep', 'IS-score = '], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    is_score, err = grep_p.communicate(input=ialign_out)
+    ialign_is_score, err = grep_p.communicate(input=ialign_out)
 
-    return float(is_score.decode())
+    ialign_is_score = ialign_is_score.decode()
+    print(ialign_is_score)
+    is_score, pvalue, z_score = [score.split('=')[-1].strip() for score in ialign_is_score.split(',')]
+    print(is_score, pvalue, z_score)
+    if is_score.isdigit():
+        return float(is_score)
+    else:
+        return 0.0
 
 
 def cluster_poses(pose_map):
