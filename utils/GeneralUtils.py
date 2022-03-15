25c859969443c485a2df1605fa5cebcda7fced3e
kylemeador <kylemeador@g.ucla.edu>
2022-03-14 19:35:18 -0700

    formatting

--- utils/GeneralUtils.py
+++ utils/GeneralUtils.py
@@ -110,23 +110,23 @@ def write_frag_match_info_file(ghost_frag=None, matched_frag=None, overlap_error
     # if not ghost_frag and not matched_frag and not overlap_error and not match_number:  # TODO
     #     raise DesignError('%s: Missing required information for writing!' % write_frag_match_info_file.__name__)
 
-    with open(os.path.join(out_path, frag_text_file), "a+") as out_info_file:
+    with open(os.path.join(out_path, frag_text_file), 'a+') as out_info_file:
         # if is_initial_match:
         if match_number == 1:
-            out_info_file.write("DOCKED POSE ID: %s\n\n" % pose_id)
-            out_info_file.write("***** ALL FRAGMENT MATCHES *****\n\n")
+            out_info_file.write('DOCKED POSE ID: %s\n\n' % pose_id)
+            out_info_file.write('***** ALL FRAGMENT MATCHES *****\n\n')
             # out_info_file.write("***** INITIAL MATCH FROM REPRESENTATIVES OF INITIAL FRAGMENT CLUSTERS *****\n\n")
         cluster_id = 'i%d_j%d_k%d' % ghost_frag.get_ijk()
-        out_info_file.write("MATCH %d\n" % match_number)
-        out_info_file.write("z-val: %f\n" % overlap_error)
-        out_info_file.write("CENTRAL RESIDUES\n")
-        out_info_file.write("oligomer1 ch, resnum: %s, %d\n" % ghost_frag.get_aligned_chain_and_residue())
-        out_info_file.write("oligomer2 ch, resnum: %s, %d\n" % matched_frag.get_central_res_tup())
-        out_info_file.write("FRAGMENT CLUSTER\n")
+        out_info_file.write('MATCH %d\n' % match_number)
+        out_info_file.write('z-val: %f\n' % overlap_error)
+        out_info_file.write('CENTRAL RESIDUES\n')
+        out_info_file.write('oligomer1 ch, resnum: %s, %d\n' % ghost_frag.get_aligned_chain_and_residue())
+        out_info_file.write('oligomer2 ch, resnum: %s, %d\n' % matched_frag.get_central_res_tup())
+        out_info_file.write('FRAGMENT CLUSTER\n')
         out_info_file.write('id: %s\n' % cluster_id)
-        out_info_file.write("mean rmsd: %f\n" % ghost_frag.get_rmsd())
-        out_info_file.write("aligned rep: int_frag_%s_%d.pdb\n" % (cluster_id, match_number))
-        out_info_file.write("central res pair freqs:\n%s\n\n" % str(central_frequencies))
+        out_info_file.write('mean rmsd: %f\n' % ghost_frag.get_rmsd())
+        out_info_file.write('aligned rep: int_frag_%s_%d.pdb\n' % (cluster_id, match_number))
+        out_info_file.write('central res pair freqs:\n%s\n\n' % str(central_frequencies))
 
         # if is_initial_match:
         #     out_info_file.write("***** ALL MATCH(ES) FROM REPRESENTATIVES OF ALL FRAGMENT CLUSTERS *****\n\n")
@@ -140,40 +140,40 @@ def write_docked_pose_info(outdir_path, res_lev_sum_score, high_qual_match_count
 
     out_info_file_path = os.path.join(outdir_path, docked_pose_file)
     with open(out_info_file_path, 'w') as out_info_file:
-        out_info_file.write("DOCKED POSE ID: %s\n\n" % pose_id)
-        out_info_file.write("Nanohedra Score: %f\n\n" % res_lev_sum_score)
-        out_info_file.write("Unique Mono Fragments Matched (z<=1): %d\n" % high_qual_match_count)
-        out_info_file.write("Unique Mono Fragments Matched: %d\n" % unique_matched_interface_monofrag_count)
-        out_info_file.write("Unique Mono Fragments at Interface: %d\n" % unique_total_interface_monofrags_count)
-        out_info_file.write("Interface Matched (%s): %f\n\n" % ("%", percent_of_interface_covered * 100))
+        out_info_file.write('DOCKED POSE ID: %s\n\n' % pose_id)
+        out_info_file.write('Nanohedra Score: %f\n\n' % res_lev_sum_score)
+        out_info_file.write('Unique Mono Fragments Matched (z<=1): %d\n' % high_qual_match_count)
+        out_info_file.write('Unique Mono Fragments Matched: %d\n' % unique_matched_interface_monofrag_count)
+        out_info_file.write('Unique Mono Fragments at Interface: %d\n' % unique_total_interface_monofrags_count)
+        out_info_file.write('Interface Matched (%s): %f\n\n' % ('%', percent_of_interface_covered * 100))
 
-        out_info_file.write("ROT/DEGEN MATRIX PDB1: %s\n" % str(rot_mat1.tolist()))
+        out_info_file.write('ROT/DEGEN MATRIX PDB1: %s\n' % str(rot_mat1.tolist()))
         if representative_int_dof_tx_param_1 is not None:
             int_dof_tx_vec_1 = representative_int_dof_tx_param_1
         else:
             int_dof_tx_vec_1 = None
-        out_info_file.write("INTERNAL Tx PDB1: %s\n" % str(int_dof_tx_vec_1))
-        out_info_file.write("SETTING MATRIX PDB1: %s\n" % str(set_mat1.tolist()))
+        out_info_file.write('INTERNAL Tx PDB1: %s\n' % str(int_dof_tx_vec_1))
+        out_info_file.write('SETTING MATRIX PDB1: %s\n' % str(set_mat1.tolist()))
         if representative_ext_dof_tx_params_1 == [0, 0, 0]:
             ref_frame_tx_vec_1 = None
         else:
             ref_frame_tx_vec_1 = representative_ext_dof_tx_params_1
-        out_info_file.write("REFERENCE FRAME Tx PDB1: %s\n\n" % str(ref_frame_tx_vec_1))
+        out_info_file.write('REFERENCE FRAME Tx PDB1: %s\n\n' % str(ref_frame_tx_vec_1))
 
-        out_info_file.write("ROT/DEGEN MATRIX PDB2: %s\n" % str(rot_mat2.tolist()))
+        out_info_file.write('ROT/DEGEN MATRIX PDB2: %s\n' % str(rot_mat2.tolist()))
         if representative_int_dof_tx_param_2 is not None:
             int_dof_tx_vec_2 = representative_int_dof_tx_param_2
         else:
             int_dof_tx_vec_2 = None
-        out_info_file.write("INTERNAL Tx PDB2: %s\n" % str(int_dof_tx_vec_2))
-        out_info_file.write("SETTING MATRIX PDB2: %s\n" % str(set_mat2.tolist()))
+        out_info_file.write('INTERNAL Tx PDB2: %s\n' % str(int_dof_tx_vec_2))
+        out_info_file.write('SETTING MATRIX PDB2: %s\n' % str(set_mat2.tolist()))
         if representative_ext_dof_tx_params_2 == [0, 0, 0]:
             ref_frame_tx_vec_2 = None
         else:
             ref_frame_tx_vec_2 = representative_ext_dof_tx_params_2
-        out_info_file.write("REFERENCE FRAME Tx PDB2: %s\n\n" % str(ref_frame_tx_vec_2))
+        out_info_file.write('REFERENCE FRAME Tx PDB2: %s\n\n' % str(ref_frame_tx_vec_2))
 
-        out_info_file.write("CRYST1 RECORD: %s\n\n" % cryst1_record)
+        out_info_file.write('CRYST1 RECORD: %s\n\n' % cryst1_record)
 
         out_info_file.write('Canonical Orientation PDB1 Path: %s\n' % pdb1_path)
         out_info_file.write('Canonical Orientation PDB2 Path: %s\n\n' % pdb2_path)
@@ -190,7 +190,7 @@ def get_rotation_step(sym_entry, rot_step_deg1=None, rot_step_deg2=None, initial
             rot_step_deg1 = 3  # set rotation step to default
     else:
         if rot_step_deg1 and initial:
-            log.warning('Specified Rotation Step 1 Was Ignored. Oligomer 1 Doesn\'t Have Internal Rotational DOF\n\n')
+            log.warning('Specified Rotation Step 1 Was Ignored. Oligomer 1 Doesn\'t Have Internal Rotational DOF\n')
         rot_step_deg1 = 1
 
     if sym_entry.is_internal_rot2:  # if rotation step required
@@ -198,37 +198,36 @@ def get_rotation_step(sym_entry, rot_step_deg1=None, rot_step_deg2=None, initial
             rot_step_deg2 = 3  # set rotation step to default
     else:
         if rot_step_deg2 and initial:
-            log.warning('Specified Rotation Step 2 Was Ignored. Oligomer 2 Doesn\'t Have Internal Rotational DOF\n\n')
+            log.warning('Specified Rotation Step 2 Was Ignored. Oligomer 2 Doesn\'t Have Internal Rotational DOF\n')
         rot_step_deg2 = 1
 
     return rot_step_deg1, rot_step_deg2
 
 
 def write_docking_parameters(pdb1_path, pdb2_path, rot_step_deg1, rot_step_deg2, sym_entry, master_outdir, log=logger):
-    log.info('NANOHEDRA PROJECT INFORMATION\n')
-    log.info('Oligomer 1 Input Directory: %s\n' % pdb1_path)
-    log.info('Oligomer 2 Input Directory: %s\n' % pdb2_path)
-    log.info('Master Output Directory: %s\n\n' % master_outdir)
-    log.info('SYMMETRY COMBINATION MATERIAL INFORMATION\n')
-    log.info('Nanohedra Entry Number: %d\n' % sym_entry.entry_number)
-    log.info('Oligomer 1 Point Group Symmetry: %s\n' % sym_entry.group1)
-    log.info('Oligomer 2 Point Group Symmetry: %s\n' % sym_entry.group2)
-    log.info('SCM Point Group Symmetry: %s\n' % sym_entry.point_group_sym)
+    log.info('NANOHEDRA PROJECT INFORMATION')
+    log.info('Oligomer 1 Input Directory: %s' % pdb1_path)
+    log.info('Oligomer 2 Input Directory: %s' % pdb2_path)
+    log.info('Master Output Directory: %s\n' % master_outdir)
+    log.info('SYMMETRY COMBINATION MATERIAL INFORMATION')
+    log.info('Nanohedra Entry Number: %d' % sym_entry.entry_number)
+    log.info('Oligomer 1 Point Group Symmetry: %s' % sym_entry.group1)
+    log.info('Oligomer 2 Point Group Symmetry: %s' % sym_entry.group2)
+    log.info('SCM Point Group Symmetry: %s' % sym_entry.point_group_sym)
     # log.info("Oligomer 1 Internal ROT DOF: %s\n" % str(sym_entry.get_internal_rot1()))
     # log.info("Oligomer 2 Internal ROT DOF: %s\n" % str(sym_entry.get_internal_rot2()))
     # log.info("Oligomer 1 Internal Tx DOF: %s\n" % str(sym_entry.get_internal_tx1()))
     # log.info("Oligomer 2 Internal Tx DOF: %s\n" % str(sym_entry.get_internal_tx2()))
-    log.info('Oligomer 1 Setting Matrix: %s\n' % sym_entry.setting_matrix1.tolist())
-    log.info('Oligomer 2 Setting Matrix: %s\n' % sym_entry.setting_matrix2.tolist())
-    log.info('Oligomer 1 Reference Frame Tx DOF: %s\n' % (sym_entry.ref_frame_tx_dof1
-                                                          if sym_entry.is_ref_frame_tx_dof1 else str(None)))
-    log.info('Oligomer 2 Reference Frame Tx DOF: %s\n' % (sym_entry.ref_frame_tx_dof2
-                                                          if sym_entry.is_ref_frame_tx_dof2 else str(None)))
-    log.info('Resulting SCM Symmetry: %s\n' % sym_entry.resulting_symmetry)
-    log.info('SCM Dimension: %d\n' % sym_entry.dimension)
-    log.info('SCM Unit Cell Specification: %s\n\n' % sym_entry.uc_specification)
-    rot_step_deg1, rot_step_deg2 = get_rotation_step(sym_entry, rot_step_deg1, rot_step_deg2, initial=True,
-                                                     log=log)
+    log.info('Oligomer 1 Setting Matrix: %s' % sym_entry.setting_matrix1.tolist())
+    log.info('Oligomer 2 Setting Matrix: %s' % sym_entry.setting_matrix2.tolist())
+    log.info('Oligomer 1 Reference Frame Tx DOF: %s' % (sym_entry.ref_frame_tx_dof1
+                                                        if sym_entry.is_ref_frame_tx_dof1 else str(None)))
+    log.info('Oligomer 2 Reference Frame Tx DOF: %s' % (sym_entry.ref_frame_tx_dof2
+                                                        if sym_entry.is_ref_frame_tx_dof2 else str(None)))
+    log.info('Resulting SCM Symmetry: %s' % sym_entry.resulting_symmetry)
+    log.info('SCM Dimension: %d' % sym_entry.dimension)
+    log.info('SCM Unit Cell Specification: %s\n' % sym_entry.uc_specification)
+    rot_step_deg1, rot_step_deg2 = get_rotation_step(sym_entry, rot_step_deg1, rot_step_deg2, initial=True, log=log)
     # # Default Rotation Step
     # if sym_entry.is_internal_rot1:  # if rotation step required
     #     if not rot_step_deg1:
@@ -246,27 +245,27 @@ def write_docking_parameters(pdb1_path, pdb2_path, rot_step_deg1, rot_step_deg2,
     #     if rot_step_deg_pdb2:
     #         log.info("Warning: Specified Rotation Step 2 Was Ignored. Oligomer 2 Doesn\'t Have"
     #                               " Internal Rotational DOF\n\n")
-    log.info('ROTATIONAL SAMPLING INFORMATION\n')
-    log.info('Oligomer 1 ROT Sampling Range: %s\n' % (str(sym_entry.rotation_range1)
-                                                       if sym_entry.is_internal_rot1 else str(None)))
-    log.info('Oligomer 2 ROT Sampling Range: %s\n' % (str(sym_entry.rotation_range2)
-                                                      if sym_entry.is_internal_rot2 else str(None)))
-    log.info('Oligomer 1 ROT Sampling Step: %s\n' % (str(rot_step_deg1) if sym_entry.is_internal_rot1
+    log.info('ROTATIONAL SAMPLING INFORMATION')
+    log.info('Oligomer 1 ROT Sampling Range: %s' % (str(sym_entry.rotation_range1)
+                                                    if sym_entry.is_internal_rot1 else str(None)))
+    log.info('Oligomer 2 ROT Sampling Range: %s' % (str(sym_entry.rotation_range2)
+                                                    if sym_entry.is_internal_rot2 else str(None)))
+    log.info('Oligomer 1 ROT Sampling Step: %s' % (str(rot_step_deg1) if sym_entry.is_internal_rot1
+                                                   else str(None)))
+    log.info('Oligomer 2 ROT Sampling Step: %s\n' % (str(rot_step_deg2) if sym_entry.is_internal_rot2
                                                      else str(None)))
-    log.info('Oligomer 2 ROT Sampling Step: %s\n\n' % (str(rot_step_deg2) if sym_entry.is_internal_rot2
-                                                       else str(None)))
     # Get Degeneracy Matrices
-    log.info('Searching For Possible Degeneracies\n')
+    log.info('Searching For Possible Degeneracies')
     if sym_entry.degeneracy_matrices_1 is None:
-        log.info('No Degeneracies Found for Oligomer 1\n')
+        log.info('No Degeneracies Found for Oligomer 1')
     elif len(sym_entry.degeneracy_matrices_1) == 1:
-        log.info('1 Degeneracy Found for Oligomer 1\n')
+        log.info('1 Degeneracy Found for Oligomer 1')
     else:
-        log.info('%d Degeneracies Found for Oligomer 1\n' % len(sym_entry.degeneracy_matrices_1))
+        log.info('%d Degeneracies Found for Oligomer 1' % len(sym_entry.degeneracy_matrices_1))
     if sym_entry.degeneracy_matrices_2 is None:
-        log.info('No Degeneracies Found for Oligomer 2\n\n')
+        log.info('No Degeneracies Found for Oligomer 2\n')
     elif len(sym_entry.degeneracy_matrices_2) == 1:
-        log.info('1 Degeneracy Found for Oligomer 2\n\n')
+        log.info('1 Degeneracy Found for Oligomer 2\n')
     else:
-        log.info('%d Degeneracies Found for Oligomer 2\n\n' % len(sym_entry.degeneracy_matrices_2))
-    log.info('Retrieving Database of Complete Interface Fragment Cluster Representatives\n')
+        log.info('%d Degeneracies Found for Oligomer 2\n' % len(sym_entry.degeneracy_matrices_2))
+    log.info('Retrieving Database of Complete Interface Fragment Cluster Representatives')
