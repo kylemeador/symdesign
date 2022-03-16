2f3f251e2566b0ed470360a036c9c1a914a4227a
kylemeador <kylemeador@g.ucla.edu>
2022-03-15 19:59:55 -0700

    debug

--- FragDock.py
+++ FragDock.py
@@ -1034,7 +1034,7 @@ def nanohedra_dock(sym_entry, ijk_frag_db, euler_lookup, master_outdir, pdb1, pd
                                          'translation2': None})
         asu_clash_counts.extend(
             [oligomer1_backbone_cb_tree.two_point_correlation(inverse_transformed_pdb2_tiled_coords[idx], [clash_dist])
-             for idx in range(inverse_transformed_pdb2_tiled_coords)])
+             for idx in range(inverse_transformed_pdb2_tiled_coords.shape[0])])
         print('asu_clash_counts: %s' % asu_clash_counts)
     check_clash_coords_time = time.time() - check_clash_coords_start
 
