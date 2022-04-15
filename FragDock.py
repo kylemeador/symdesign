88807a49bd58e0cd9231d45ee02159006abec91a
kylemeador <kylemeador@g.ucla.edu>
2022-04-15 12:41:23 -0700

    debug

--- FragDock.py
+++ FragDock.py
@@ -1110,36 +1110,6 @@ def nanohedra_dock(sym_entry, ijk_frag_db, euler_lookup, master_outdir, pdb1, pd
         # print('asu_clash_counts: %s' % asu_clash_counts)
     check_clash_coords_time = time.time() - check_clash_coords_start
 
-    # # check of transformation with forward of 2 and reverse of 1
-    # pdb1.write(out_path=os.path.join(os.getcwd(), 'TEST_forward_reverse_pdb1.pdb'))
-    # for idx in range(5):
-    #     # print(full_rotation2[idx].shape)
-    #     # print(full_int_tx2[idx].shape)
-    #     # print(set_mat2.shape)
-    #     # print(full_ext_tx_sum[idx].shape if full_ext_tx_sum else None)
-    #     pdb2_copy = pdb2.return_transformed_copy(**{'rotation': full_rotation2[idx],
-    #                                                 'translation': full_int_tx2[idx],
-    #                                                 'rotation2': set_mat2,
-    #                                                 'translation2': full_ext_tx_sum[idx]
-    #                                                 if full_ext_tx_sum is not None else None})
-    #     # pdb2_copy.write(out_path=os.path.join(os.getcwd(), 'TEST_forward_reverse_transform_mid%d.pdb' % idx))
-    #     pdb2_copy.transform(**{'rotation': inv_setting1,
-    #                            'translation': full_int_tx1[idx] * -1,
-    #                            'rotation2': full_inv_rotation1[idx]})
-    #     pdb2_copy.write(out_path=os.path.join(os.getcwd(), 'TEST_forward_reverse_transform%d.pdb' % idx))
-    #
-    # for idx in range(5):
-    #     pdb1_copye = pdb1.return_transformed_copy(**{'rotation': full_rotation1[idx],
-    #                                                  'translation': full_int_tx1[idx],
-    #                                                  'rotation2': set_mat1,
-    #                                                  'translation2': full_ext_tx1[idx] if full_ext_tx1 is not None else None})
-    #     pdb1_copye.write(out_path=os.path.join(os.getcwd(), 'TEST_forward_transform1_%d.pdb' % idx))
-    #     pdb2_copye = pdb2.return_transformed_copy(**{'rotation': full_rotation2[idx],
-    #                                                  'translation': full_int_tx2[idx],
-    #                                                  'rotation2': set_mat2,
-    #                                                  'translation2': full_ext_tx2[idx] if full_ext_tx2 is not None else None})
-    #     pdb2_copye.write(out_path=os.path.join(os.getcwd(), 'TEST_forward_transform2_%d.pdb' % idx))
-
     # asu_is_viable = np.where(asu_clash_counts.flatten() == 0)  # , True, False)
     # asu_is_viable = np.where(np.array(asu_clash_counts) == 0)
     asu_is_viable = np.where(asu_clash_counts == 0)
@@ -1166,6 +1136,36 @@ def nanohedra_dock(sym_entry, ijk_frag_db, euler_lookup, master_outdir, pdb1, pd
 
     full_inv_rotation1 = full_inv_rotation1[asu_is_viable]
     # viable_cluster_labels = cluster_labels[asu_is_viable[0]]
+
+    # check of transformation with forward of 2 and reverse of 1
+    pdb1.write(out_path=os.path.join(os.getcwd(), 'TEST_forward_reverse_pdb1.pdb'))
+    for idx in range(5):
+        # print(full_rotation2[idx].shape)
+        # print(full_int_tx2[idx].shape)
+        # print(set_mat2.shape)
+        # print(full_ext_tx_sum[idx].shape if full_ext_tx_sum else None)
+        pdb2_copy = pdb2.return_transformed_copy(**{'rotation': full_rotation2[idx],
+                                                    'translation': full_int_tx2[idx],
+                                                    'rotation2': set_mat2,
+                                                    'translation2': full_ext_tx_sum[idx]
+                                                    if full_ext_tx_sum is not None else None})
+        # pdb2_copy.write(out_path=os.path.join(os.getcwd(), 'TEST_forward_reverse_transform_mid%d.pdb' % idx))
+        pdb2_copy.transform(**{'rotation': inv_setting1,
+                               'translation': full_int_tx1[idx] * -1,
+                               'rotation2': full_inv_rotation1[idx]})
+        pdb2_copy.write(out_path=os.path.join(os.getcwd(), 'TEST_forward_reverse_transform%d.pdb' % idx))
+
+    for idx in range(5):
+        pdb1_copye = pdb1.return_transformed_copy(**{'rotation': full_rotation1[idx],
+                                                     'translation': full_int_tx1[idx],
+                                                     'rotation2': set_mat1,
+                                                     'translation2': full_ext_tx1[idx] if full_ext_tx1 is not None else None})
+        pdb1_copye.write(out_path=os.path.join(os.getcwd(), 'TEST_forward_transform1_%d.pdb' % idx))
+        pdb2_copye = pdb2.return_transformed_copy(**{'rotation': full_rotation2[idx],
+                                                     'translation': full_int_tx2[idx],
+                                                     'rotation2': set_mat2,
+                                                     'translation2': full_ext_tx2[idx] if full_ext_tx2 is not None else None})
+        pdb2_copye.write(out_path=os.path.join(os.getcwd(), 'TEST_forward_transform2_%d.pdb' % idx))
 
     #################
     # Query PDB1 CB Tree for all PDB2 CB Atoms within "cb_distance" in A of a PDB1 CB Atom
