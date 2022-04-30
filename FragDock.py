0a1c476067a010b9e267c0046fe9e7d51f24b024
kylemeador <kylemeador@g.ucla.edu>
2022-04-29 17:33:56 -0700

    brief Todd docking test

--- FragDock.py
+++ FragDock.py
@@ -549,18 +549,31 @@ def nanohedra_dock(sym_entry, ijk_frag_db, euler_lookup, master_outdir, pdb1, pd
 
     get_complete_ghost_frags1_time_stop = time.time()
     #################################
-    # TODO write out frags
-    # guide_file_ghost = '/home/kmeador/c1_overlap_files/1nu4_ghost_coords.txt'
-    # with open(guide_file_ghost, 'w') as f:
-    #     for coord_group in ghost_frag1_guide_coords.tolist():
-    #         f.write('%s\n' % ' '.join('%f,%f,%f' % tuple(coords) for coords in coord_group))
-    # guide_file_ghost_idx = '/home/kmeador/c1_overlap_files/1nu4_ghost_coords_index.txt'
-    # with open(guide_file_ghost_idx, 'w') as f:
-    #     f.write('%s\n' % '\n'.join(map(str, ghost_frag1_j_indices.tolist())))
-    # guide_file_ghost_res_num = '/home/kmeador/c1_overlap_files/1nu4_ghost_coords_residue_number.txt'
-    # with open(guide_file_ghost_res_num, 'w') as f:
-    #     f.write('%s\n' % '\n'.join(map(str, ghost_frag1_residues.tolist())))
-    #
+    # TODO REMOVE write out frags
+    guide_file_ghost = os.path.join(os.getcwd(), '%s_ghost_coords.txt' % pdb1.name)
+    with open(guide_file_ghost, 'w') as f:
+        for coord_group in ghost_frag1_guide_coords.tolist():
+            f.write('%s\n' % ' '.join('%f,%f,%f' % tuple(coords) for coords in coord_group))
+    guide_file_ghost_idx = os.path.join(os.getcwd(), '%s_ghost_coords_index.txt' % pdb1.name)
+    with open(guide_file_ghost_idx, 'w') as f:
+        f.write('%s\n' % '\n'.join(map(str, ghost_frag1_j_indices.tolist())))
+    guide_file_ghost_res_num = os.path.join(os.getcwd(), '%s_ghost_coords_residue_number.txt' % pdb1.name)
+    with open(guide_file_ghost_res_num, 'w') as f:
+        f.write('%s\n' % '\n'.join(map(str, ghost_frag1_residues.tolist())))
+
+    guide_file_surf = os.path.join(os.getcwd(), '%s_surf_coords.txt' % pdb2.name)
+    with open(guide_file_surf, 'w') as f:
+        for coord_group in surf_frags2_guide_coords.tolist():
+            f.write('%s\n' % ' '.join('%f,%f,%f' % tuple(coords) for coords in coord_group))
+    guide_file_surf_idx = os.path.join(os.getcwd(), '%s_surf_coords_index.txt' % pdb2.name)
+    with open(guide_file_surf_idx, 'w') as f:
+        f.write('%s\n' % '\n'.join(map(str, surf_frags2_i_indices.tolist())))
+    guide_file_surf_res_num = os.path.join(os.getcwd(), '%s_surf_coords_residue_number.txt' % pdb2.name)
+    with open(guide_file_surf_res_num, 'w') as f:
+        f.write('%s\n' % '\n'.join(map(str, surf_frag2_residues.tolist())))
+
+    raise RuntimeError('Suspending operation of %s/%s after write' % (pdb1.name, pdb2.name))
+
     ij_type_match_lookup_table = compute_ij_type_lookup(ghost_frag1_j_indices, surf_frags2_i_indices)
     # ^ axis 0 is ghost frag, 1 is surface frag
     # ij_matching_ghost1_indices = \
