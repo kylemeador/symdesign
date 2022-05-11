4d332c7e39edc0c5163728bafabda285397ccccb
kylemeador <kylemeador@g.ucla.edu>
2022-05-11 12:08:10 -0700

    consolidate to single script

--- ExpandASU.py
+++ ExpandASU.py
@@ -1,19 +1,44 @@
 import argparse
 import os
+from typing import Union
 
 from PDB import PDB
 from SymDesignUtils import start_log, to_iterable, set_logging_to_debug
-from utils.SymmetryUtils import get_ptgrp_sym_op, get_expanded_ptgrp_pdb, cubic_point_groups  # , write_expanded_ptgrp
+from utils.SymmetryUtils import cubic_point_groups, point_group_symmetry_operators
 
 
-def expand_asu(file, symmetry, out_path=None):
+def get_expanded_ptgrp_pdb(pdb_asu, expand_matrices):
+    """Returns a list of PDB objects from the symmetry mates of the input expansion matrices"""
+    asu_symm_mates = []
+    # asu_coords = pdb_asu.extract_coords()
+    # asu_coords = pdb_asu.extract_all_coords()
+    for r in expand_matrices:
+        asu_sym_mate_pdb = pdb_asu.return_transformed_copy(rotation=r.T)
+        asu_symm_mates.append(asu_sym_mate_pdb)
+
+    return asu_symm_mates
+
+
+def write_expanded_ptgrp(expanded_ptgrp_pdbs, outfile_path):
+    outfile = open(outfile_path, "w")
+    model_count = 1
+    for pdb in expanded_ptgrp_pdbs:
+        outfile.write("MODEL     {:>4s}\n".format(str(model_count)))
+        model_count += 1
+        for atom in pdb.all_atoms:
+            outfile.write(str(atom))
+        outfile.write("ENDMDL\n")
+    outfile.close()
+
+
+def expand_asu(file, symmetry, out_path=None) -> Union[str, bytes]:
     if out_path:
         path_name = os.path.join(out_path, '%s_%s.pdb' % (os.path.basename(os.path.splitext(file)[0]), 'expanded'))
     else:
         path_name = '%s_%s.pdb' % (os.path.splitext(file)[0], 'expanded')
 
     asu_pdb = PDB(file=file)
-    expand_matrices = get_ptgrp_sym_op(symmetry.upper())  # currently only works for T, O, I
+    expand_matrices = point_group_symmetry_operators[symmetry.upper()]
     expanded_pdb = get_expanded_ptgrp_pdb(asu_pdb, expand_matrices)
     write_expanded_ptgrp(expanded_pdb, path_name)
 
