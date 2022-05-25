b95890f6b6019c9f701c764690f8cbb7155c7471
kylemeador <kylemeador@g.ucla.edu>
2022-05-24 23:33:10 -0700

    debug and finalize

--- tools/ConcatenatePDBFiles.py
+++ tools/ConcatenatePDBFiles.py
@@ -2,7 +2,11 @@ import argparse
 import os
 
 from PDB import PDB
-from SymDesignUtils import get_all_file_paths
+from SymDesignUtils import get_all_file_paths, start_log
+
+# globals
+logger = start_log(name=os.path.basename(__file__))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Concatenate a group of structure files. The name of the resulting '
@@ -19,9 +23,9 @@ if __name__ == '__main__':
     if args.directory:
         args.files = get_all_file_paths(args.directory, extension='.pdb')
     # initialize PDB Objects
-    pdbs = [PDB.from_file(pdb_file, log=None) for pdb_file in args.files]
-    pdb = PDB.from_chains([pdb.chain for pdb in pdbs], name=''.join(pdb.name for pdb in pdbs), log=None, entities=False)
-
+    pdbs = [PDB.from_file(pdb_file, log=logger) for pdb_file in args.files]
+    pdb = PDB.from_chains([chain for pdb in pdbs for chain in pdb.chains], name='-'.join(pdb.name for pdb in pdbs),
+                          log=logger, entities=False)
     # output file
     os.makedirs(args.output_directory, exist_ok=True)
-    pdb.write(out_path=os.path.join(args.output_directory, f'{pdb.name}.pdb'), increment_chains=True)
+    pdb.write(out_path=os.path.join(args.output_directory, f'{pdb.name}.pdb'))
