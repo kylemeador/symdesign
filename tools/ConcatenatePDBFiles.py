58b0c2f558ef41cb65034bc9f391bfe5eb055008
kylemeador <kylemeador@g.ucla.edu>
2022-05-24 18:38:27 -0700

    ensure correct formatting of I/O

--- tools/ConcatenatePDBFiles.py
+++ tools/ConcatenatePDBFiles.py
@@ -11,12 +11,17 @@ if __name__ == '__main__':
     input_mutex = parser.add_mutually_exclusive_group(required=True)
     input_mutex.add_argument('-f', '--files', nargs='+', help='PDB files to be concatenated', default=[])
     input_mutex.add_argument('-d', '--directory', help='A directory with the PDB files to be concatenated')
-    parser.add_argument('-Od', '--output_directory', required=True,
+    parser.add_argument('-Od', '--output_directory', type=os.path.abspath, required=True,
                         help='The directory where the concatenated PDB files should be written')
     args, additional_args = parser.parse_known_args()
 
+    # input files
     if args.directory:
         args.files = get_all_file_paths(args.directory, extension='.pdb')
+    # initialize PDB Objects
     pdbs = [PDB.from_file(pdb_file, log=None) for pdb_file in args.files]
     pdb = PDB.from_chains([pdb.chain for pdb in pdbs], name=''.join(pdb.name for pdb in pdbs), log=None, entities=False)
+
+    # output file
+    os.makedirs(args.output_directory, exist_ok=True)
     pdb.write(out_path=os.path.join(args.output_directory, f'{pdb.name}.pdb'), increment_chains=True)
