d3a3a19b6e55ad275daa5734b27e61c57b89f91d
kylemeador <kylemeador@g.ucla.edu>
2022-06-22 00:57:57 -0700

    refactor argument parsing

--- dependencies/bin/models_to_multimodel.py
+++ dependencies/bin/models_to_multimodel.py
@@ -1,17 +1,16 @@
-import os
-from glob import glob
-import argparse
+from os import path
+from argparse import ArgumentParser
 
 from SymDesignUtils import get_directory_file_paths
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Create a list of files the belong in a directory')
-    parser.add_argument('-d', '--directory', type=os.path.abspath, help='Directory where models are located',
+    parser = ArgumentParser(description='Create a list of files the belong in a directory')
+    parser.add_argument('-d', '--directory', type=path.abspath, help='Directory where models are located',
                         required=True)
-    parser.add_argument('-s', '--suffix', type=str, help='A suffix in the files to search for.', default='')
+    parser.add_argument('-s', '--suffix', type=str, help='A suffix in the files to search for', default='')
     parser.add_argument('-e', '--extension', type=str, help='An extension in the files to search for.')
-    parser.add_argument('-o', '--out_path', type=os.path.abspath, help='File where multimodel should be written.',
+    parser.add_argument('-o', '--out_path', type=path.abspath, help='File where multimodel should be written.',
                         required=True)
 
     args, additional_args = parser.parse_known_args()
