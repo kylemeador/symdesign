16f937aa7c9f1a7890f2c81c9b676587c3f9919c
kylemeador <kylemeador@g.ucla.edu>
2022-07-21 23:55:47 -0700

    remove debugging statements

--- Structure.py
+++ Structure.py
@@ -2148,9 +2148,6 @@ class Residue(ResidueFragment, ContainsAtomsMixin):
             for atom in self.atoms:
                 polarity_list[residue_atom_polarity.get(atom.type)].append(atom.sasa)
         except AttributeError:  # missing atom.sasa
-            # Todo remove below
-            print(atom.number, atom.type, atom.residue_type, atom.residue_number)
-            exit()
             self.parent.get_sasa()
             for atom in self.atoms:
                 polarity_list[residue_atom_polarity.get(atom.type)].append(atom.sasa)
@@ -4112,11 +4109,6 @@ class Structure(ContainsAtomsMixin):  # Todo Polymer?
             # slice removes first REMARK, MODEL and final TER, MODEL regardless of # of chains, TER inclusion
             # since return_atom_record doesn't have models, these won't be present and no option to freesasa about model
             # would be provided with above subprocess call
-            # Todo remove below
-            print(self.return_atom_record())
-            print('cmd:', subprocess.list2cmdline([free_sasa_exe_path, f'--format={out_format}', '--probe-radius', str(probe_radius),
-                                                   '-c', free_sasa_configuration_path] + include_hydrogen))
-            print('\n'.join(sasa_output))
             atoms = self.atoms
             for line_split in map(str.split, sasa_output[5:-2]):  # slice could remove need for if ATOM
                 if line_split[0] == 'ATOM':  # this seems necessary as MODEL can be added if MODEL is written
