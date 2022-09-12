83f0a58e681d5bf79fd6c0c0331cab564e9ebc0d
kylemeador <kylemeador@g.ucla.edu>
2022-09-12 01:18:02 -0700

    debug

--- protocols.py
+++ protocols.py
@@ -1294,6 +1294,7 @@ class PoseDirectory:
         # try:
         # Todo make better patch for numpy.ndarray compare value of array is ambiguous
         if self.info.keys() != self._info.keys():  # if the state has changed from the original version
+            print('info', self.info)
             pickle_object(self.info, self.serialized_info, out_path='')
         # except ValueError:
         #     print(self.info)
