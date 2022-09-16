71ba6b17f41fd43b2f8e2ed045e52d9bd20bafde
kylemeador <kylemeador@g.ucla.edu>
2022-09-15 17:18:15 -0700

    refactor sequence translation tables

--- structure/utils.py
+++ structure/utils.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from collections import defaultdict
 from typing import get_args, Literal
 
 protein_letters3: tuple[str, ...] = \
@@ -33,3 +34,37 @@ protein_letters_alph3_gapped_literal = \
     Literal['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', '-']
 protein_letters_alph3_unknown_gapped_literal = \
     Literal['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'X', '-']
+numerical_translation_alph1 = defaultdict(lambda: 20, zip(protein_letters_alph1,
+                                                          range(len(protein_letters_alph1))))
+numerical_translation_alph3 = defaultdict(lambda: 20, zip(protein_letters_alph3,
+                                                          range(len(protein_letters_alph3))))
+numerical_translation_alph1_bytes = defaultdict(lambda: 20, zip([item.encode() for item in protein_letters_alph1],
+                                                                range(len(protein_letters_alph1))))
+numerical_translation_alph3_bytes = defaultdict(lambda: 20, zip([item.encode() for item in protein_letters_alph3],
+                                                                range(len(protein_letters_alph3))))
+sequence_translation_alph1 = defaultdict(lambda: '-', zip(range(len(protein_letters_alph1)), protein_letters_alph1))
+sequence_translation_alph3 = defaultdict(lambda: '-', zip(range(len(protein_letters_alph3)), protein_letters_alph3))
+numerical_translation_alph1_gapped = defaultdict(lambda: 20, zip(protein_letters_alph1_gapped,
+                                                                 range(len(protein_letters_alph1_gapped))))
+numerical_translation_alph3_gapped = defaultdict(lambda: 20, zip(protein_letters_alph3_gapped,
+                                                                 range(len(protein_letters_alph3_gapped))))
+numerical_translation_alph1_gapped_bytes = \
+    defaultdict(lambda: 20, zip([item.encode() for item in protein_letters_alph1_gapped],
+                                range(len(protein_letters_alph1_gapped))))
+numerical_translation_alph3_gapped_bytes = \
+    defaultdict(lambda: 20, zip([item.encode() for item in protein_letters_alph3_gapped],
+                                range(len(protein_letters_alph3_gapped))))
+numerical_translation_alph1_unknown_bytes = \
+    defaultdict(lambda: 20, zip([item.encode() for item in protein_letters_alph1_unknown],
+                                range(len(protein_letters_alph1_unknown))))
+numerical_translation_alph3_unknown_bytes = \
+    defaultdict(lambda: 20, zip([item.encode() for item in protein_letters_alph3_unknown],
+                                range(len(protein_letters_alph1_unknown))))
+numerical_translation_alph1_unknown_gapped_bytes = \
+    defaultdict(lambda: 20, zip([item.encode() for item in protein_letters_alph1_unknown_gapped],
+                                range(len(protein_letters_alph1_unknown_gapped))))
+numerical_translation_alph3_unknown_gapped_bytes = \
+    defaultdict(lambda: 20, zip([item.encode() for item in protein_letters_alph3_unknown_gapped],
+                                range(len(protein_letters_alph1_unknown_gapped))))
+extended_protein_letters_and_gap_literal = Literal[get_args(protein_letters_alph1_extended_literal), '-']
+extended_protein_letters_and_gap: tuple[str, ...] = get_args(extended_protein_letters_and_gap_literal)
