6ad9e1c5f67529b12bb635e76b95001fb4d87541
kylemeador <kylemeador@g.ucla.edu>
2022-09-21 19:45:26 -0700

    update calculate_profile indexing

--- structure/sequence.py
+++ structure/sequence.py
@@ -1443,11 +1443,16 @@ class SequenceProfile:
                           f'\n\t%s'
                           % '\n\t'.join(f'Residue {entry + 1:5d}: {weight * 100:.0f}% fragment weight'
                                         for entry, weight in self.alpha.items()))
+        print('before', self.profile)
         for entry, weight in self.alpha.items():  # Weight will be 0 if the fragment_profile is empty
-            for aa in protein_letters_alph1:
-                self.profile[entry][aa] = \
-                    (weight*self.fragment_profile[entry][aa])\
-                    + ((1-weight) * self.profile[entry + zero_offset][aa])
+            inverse_weight = 1 - weight
+            frag_profile_entry = self.fragment_profile[entry]
+            profile_entry = self.profile[entry + zero_offset]
+            profile_entry.update({aa: weight*frag_profile_entry[aa] + inverse_weight*profile_entry[aa]
+                                  for aa in protein_letters_alph1})
+            # for aa in protein_letters_alph1:
+            #     profile_entry[aa] = weight*frag_profile_entry[aa] + inverse_weight*profile_entry[aa]
+        print('after', self.profile)
 
         if favor_fragments:
             boltzman_energy = 1
