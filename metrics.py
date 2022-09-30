3da11da4357d005169df29ec5d88d601bd686817
kylemeador <kylemeador@g.ucla.edu>
2022-09-29 19:59:44 -0700

    debug

--- metrics.py
+++ metrics.py
@@ -2108,7 +2108,7 @@ def hydrophobic_collapse_index(sequence: Sequence[str | int] | np.ndarry, hydrop
             # torch.Tensor and np.ndarray can multiply by np.ndarray
             values = np.array([hydrophobicity_values[aa] for aa in alphabet])
             if sequence.ndim == 2:
-                print('array.shape', sequence.shape, 'values.shape', values.shape)
+                # print('array.shape', sequence.shape, 'values.shape', values.shape)
                 # The array must have shape (number_of_residues, alphabet_length)
                 sequence_array = sequence * values
                 # Ensure each position is a combination of the values for each amino acid
