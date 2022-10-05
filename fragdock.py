2228b23c2d321908b458702de0c3eff3a97149e5
kylemeador <kylemeador@g.ucla.edu>
2022-10-05 01:10:58 -0700

    remove min on sampling size

--- fragdock.py
+++ fragdock.py
@@ -1349,12 +1349,12 @@ def nanohedra_dock(sym_entry: SymEntry, root_out_dir: AnyStr, model1: Structure 
     # Todo resolve. Below uses eulerints
     # Get rotated Oligomer1 Ghost Fragment, Oligomer2 Surface Fragment guide coodinate pairs
     # in the same Euler rotational space bucket
-    for idx1 in range(min(rotation_matrices1.shape[0], 13)):  # rotation_matrices1.shape[0]):  # Todo remove min
+    for idx1 in range(rotation_matrices1.shape[0]):  # min(rotation_matrices1.shape[0], 13)):  # Todo remove min
         rot1_count = idx1%number_of_rotations1 + 1
         degen1_count = idx1//number_of_rotations1 + 1
         rot_mat1 = rotation_matrices1[idx1]
         rotation_ghost_euler_ints1 = stacked_ghost_euler_int1[idx1]
-        for idx2 in range(min(rotation_matrices2.shape[0], 12)):  # rotation_matrices2.shape[0]):  # Todo remove min
+        for idx2 in range(rotation_matrices2.shape[0]):  # min(rotation_matrices2.shape[0], 12)):  # Todo remove min
             # Rotate Oligomer2 Surface and Ghost Fragment Guide Coordinates using rot_mat2 and set_mat2
             rot2_count = idx2%number_of_rotations2 + 1
             degen2_count = idx2//number_of_rotations2 + 1
