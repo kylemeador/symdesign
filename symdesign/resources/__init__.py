1c5092db3c4a4fd153223eadd7bfe4b88b8a4277
kylemeador <kylemeador@g.ucla.edu>
2022-11-06 17:56:33 -0800

    comments

--- symdesign/resources/__init__.py
+++ symdesign/resources/__init__.py
@@ -1,2 +1,4 @@
+# from . import job  # Doesn't work due to circular imports
+# from . import ml  # Doesn't work due to circular imports
 # from . import wrapapi  # Doesn't work due to circular imports
 # from . import structure_db  # Doesn't work due to circular imports
