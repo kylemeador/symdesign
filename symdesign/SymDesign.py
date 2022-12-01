39ba370e3763536b18959d9ca43d7af134b2cc7b
kylemeador <kylemeador@g.ucla.edu>
2022-12-01 13:47:02 -0800

    refactoring load_total_dataframe

--- symdesign/SymDesign.py
+++ symdesign/SymDesign.py
@@ -292,47 +292,6 @@ def main():
         # print('\n')
         exit(exit_code)
 
-    def load_total_dataframe(pose_directories: Iterable[PoseDirectory], pose: bool = False) -> pd.DataFrame:
-        """Return a pandas DataFrame with the trajectories of every pose_directory loaded and formatted according to the
-        design directory and design on the index
-
-        Args:
-            pose_directories: The pose_directories for which metrics are desired
-            pose: Whether the total dataframe should contain the mean metrics from the pose or each individual design
-        """
-        # global pose_directories
-        # global results
-        all_dfs = []  # None for design in pose_directories]
-        for idx, pose_dir in enumerate(pose_directories):
-            try:
-                all_dfs.append(pd.read_csv(pose_dir.trajectories, index_col=0, header=[0]))
-            except FileNotFoundError:  # as error
-                # results[idx] = error
-                logger.warning(f'{pose_dir}: No trajectory analysis file found. Skipping')
-
-        if pose:
-            for idx, df in enumerate(all_dfs):
-                df.fillna(0., inplace=True)  # Shouldn't be necessary if saved files were formatted correctly
-                # try:
-                df.drop([index for index in df.index.to_list() if isinstance(index, float)], inplace=True)
-                # Get rid of all individual trajectories and std, not mean
-                pose_name = pose_directories[idx].name
-                df.drop([index for index in df.index.to_list() if pose_name in index or 'std' in index], inplace=True)
-                # except TypeError:
-                #     for index in df.index.to_list():
-                #         print(index, type(index))
-        else:  # designs
-            for idx, df in enumerate(all_dfs):
-                # Get rid of all statistic entries, mean, std, etc.
-                pose_name = pose_directories[idx].name
-                df.drop([index for index in df.index.to_list() if pose_name not in index], inplace=True)
-
-        # Add pose directory str as MultiIndex
-        df = pd.concat(all_dfs, keys=[str(pose_dir) for pose_dir in pose_directories])
-        df.replace({False: 0, True: 1, 'False': 0, 'True': 1}, inplace=True)
-
-        return df
-
     def get_sym_entry_from_nanohedra_directory(nanohedra_dir: AnyStr) -> utils.SymEntry.SymEntry:
         """Handles extraction of Symmetry info from Nanohedra outputs.
 
@@ -1443,7 +1402,7 @@ def main():
         if job.specification_file:
             loc_result = [(pose_directory, design) for pose_directory in pose_directories
                           for design in pose_directory.specific_designs]
-            total_df = load_total_dataframe(pose_directories)
+            total_df = protocols.load_total_dataframe(pose_directories)
             selected_poses_df = \
                 metrics.prioritize_design_indices(total_df.loc[loc_result, :], filter=job.filter, weight=job.weight,
                                                   protocol=job.protocol, function=job.weight_function)
@@ -1460,7 +1419,7 @@ def main():
             # results = {pose_directory: results[str(pose_directory)] for pose_directory in pose_directories
             #            if str(pose_directory) in results}
         elif job.total:
-            total_df = load_total_dataframe(pose_directories)
+            total_df = protocols.load_total_dataframe(pose_directories)
             if job.protocol:
                 group_df = total_df.groupby('protocol')
                 df = pd.concat([group_df.get_group(x) for x in group_df.groups], axis=1,
@@ -1542,7 +1501,7 @@ def main():
             # Todo there is no sort here so the select_number isn't really doing anything
             results = {pose_dir: designs for pose_dir, designs in list(results.items())[:job.select_number]}
             loc_result = [(pose_dir, design) for pose_dir, designs in results.items() for design in designs]
-            total_df = load_total_dataframe(pose_directories)
+            total_df = protocols.load_total_dataframe(pose_directories)
             save_poses_df = total_df.loc[loc_result, :].droplevel(0).droplevel(0, axis=1).droplevel(0, axis=1)
 
         # Format selected sequences for output
