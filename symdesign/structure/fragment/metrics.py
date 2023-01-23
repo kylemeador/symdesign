fa4711950233b5463ad672664a3f4b1f63579e7b
kylemeador <kylemeador@g.ucla.edu>
2023-01-23 13:27:42 -0800

    debug center_residue_indices for multiple protocol with deepcopy()

--- symdesign/structure/fragment/metrics.py
+++ symdesign/structure/fragment/metrics.py
@@ -3,11 +3,17 @@ from __future__ import annotations
 import numpy as np
 
 fragment_metric_template = \
-    {'center_indices': set(), 'total_indices': set(),
-     'nanohedra_score': 0., 'nanohedra_score_center': 0., 'multiple_fragment_ratio': 0.,
-     'number_fragment_residues_total': 0, 'number_fragment_residues_center': 0,
-     'number_of_fragments': 0, 'percent_fragment_helix': 0., 'percent_fragment_strand': 0.,
-     'percent_fragment_coil': 0.}
+    dict(center_indices=set(),
+         total_indices=set(),
+         nanohedra_score=0.,
+         nanohedra_score_center=0.,
+         multiple_fragment_ratio=0.,
+         number_fragment_residues_total=0,
+         number_fragment_residues_center=0,
+         number_of_fragments=0,
+         percent_fragment_helix=0.,
+         percent_fragment_strand=0.,
+         percent_fragment_coil=0.)
 
 
 # @njit
