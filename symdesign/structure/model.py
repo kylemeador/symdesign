5512b0add60e52861590149c3a33d77d3ad9fce8
kylemeador <kylemeador@g.ucla.edu>
2023-01-23 01:05:02 -0800

    debug parsing rosetta scores

--- symdesign/structure/model.py
+++ symdesign/structure/model.py
@@ -7746,6 +7746,7 @@ class Pose(SymmetricModel, Metrics):
                      ...},
                  ...}
         """
+        res_slice = slice(0, 4)
         energy_template = {'complex': 0., 'bound': [0. for _ in self.entities], 'unbound': [0. for _ in self.entities],
                            'solv_complex': 0., 'solv_bound': [0. for _ in self.entities],
                            'solv_unbound': [0. for _ in self.entities], 'fsp': 0., 'cst': 0.}
@@ -7762,17 +7763,20 @@ class Pose(SymmetricModel, Metrics):
         for design, scores in design_scores.items():
             residue_data = {}
             for key, value in scores.items():
-                if key[:4] != 'res_':  # 'res_' not in key:  # if not key.startswith('per_res_'):
+                if key[res_slice] != 'res_':  # 'res_' not in key:  # if not key.startswith('per_res_'):
                     continue
                 # per_res_energysolv_complex_15W or per_res_energysolv_2_bound_415B
-                res, metric, entity_or_complex, *_ = metadata = key.strip('_').split('_')
-                # metadata = key.strip('_').split('_')
-                # metric = metadata[1]  # energy [or sasa]
-                # entity_or_complex = metadata[2]  # 1,2,3,... or complex
-                # remove chain_id in rosetta_numbering="False"
-                # if we have enough chains, weird chain characters appear "per_res_energy_complex_19_" which mess up
-                # split. Also numbers appear, "per_res_energy_complex_1161" which may indicate chain "1" or residue 1161
-                residue_number = int(metadata[-1].translate(utils.keep_digit_table))
+                try:
+                    res, metric, entity_or_complex, *_ = metadata = key.strip('_').split('_')
+                    # metadata = key.strip('_').split('_')
+                    # metric = metadata[1]  # energy [or sasa]
+                    # entity_or_complex = metadata[2]  # 1,2,3,... or complex
+                    # remove chain_id in rosetta_numbering="False"
+                    # if we have enough chains, weird chain characters appear "per_res_energy_complex_19_" which mess up
+                    # split. Also numbers appear, "per_res_energy_complex_1161" which may indicate chain "1" or residue 1161
+                    residue_number = int(metadata[-1].translate(utils.keep_digit_table))
+                except Exception as error:
+                    raise DesignError(f"Found the following error: {error}\n{key}")
                 if any(residue_number > last_residue for last_residue in c_term_residue_numbers):  # pose_length:
                     if warn:
                         warn = False
