f4fbbb814f9d18e796fdb641415ebe927af00d10
kylemeador <kylemeador@g.ucla.edu>
2023-03-24 15:06:49 -0700

    modernize tool

--- symdesign/tools/format_pose_ids.py
+++ symdesign/tools/format_pose_ids.py
@@ -4,6 +4,7 @@ import re
 from csv import reader
 from itertools import repeat
 
+from symdesign import flags
 from symdesign import utils
 
 
@@ -11,16 +12,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create a list of files that should be grouped into a continuous job')
     # parser.add_argument('-d', '--directory', type=os.path.abspath, help='Directory where models are located',
     #                     required=True)
-    parser.add_argument('-f', '--file', type=os.path.abspath, help='File where pose IDs are located', required=True)
+    parser.add_argument(*flags.file_args, type=os.path.abspath, help='File where pose IDs are located', required=True)
+    parser.add_argument(*flags.output_file_args, type=os.path.abspath,
+                        help='File where pose IDs should be written')
     # parser.add_argument('-s', '--suffix', type=str, help='A suffix in the files to search for.', default='')
     # parser.add_argument('-o', '--out_path', type=os.path.abspath, help='Directory where list should be written.',
     #                     required=True)
-    parser.add_argument('-k', '--keep_design_id', action='store_true',
+    parser.add_argument('-k', '--keep-design-id', action='store_true',
                         help='Whether to keep a Design ID appended to the Pose ID')
-    parser.add_argument('-s', '--split_design_id', action='store_true',
+    parser.add_argument('-s', '--split-design-id', action='store_true',
                         help='Whether an additional design name should be split into pose, design pairs')
     parser.add_argument('-p', '--project', type=str, help='Add a project directory to the Pose IDs')
-    parser.add_argument('-E', '--include_extra_info', action='store_true',
+    parser.add_argument('-E', '--include-extra-info', action='store_true',
                         help='Whether to include extra file info in the returned pose IDs')
     subparsers = parser.add_subparsers(title='Modules', dest='module',
                                        description='These are the different modes that Pose IDs can be processed',
@@ -29,7 +32,7 @@ if __name__ == '__main__':
     # ---------------------------------------------------
     filter_parser = subparsers.add_parser('filter', help='Filter Pose IDs from file by a selection file to retrieve '
                                                          'poses of interest.')
-    filter_parser.add_argument('-sf', '--selection_id_file', type=str,
+    filter_parser.add_argument('-sf', '--selection-id-file', type=str,
                                help='The file used to filter (select) IDs of interest')
     # ---------------------------------------------------
     extract_parser = subparsers.add_parser('extract', help='Extract Pose IDs from file for poses of interest.')
@@ -59,14 +62,14 @@ if __name__ == '__main__':
     else:
         project = ''
 
-    # extract only pose_ids from the input pose_id_lines
-    pose_ids = list(filter(re.compile('.*(\w{4})_(\w{4})[/-]'
-                                      '.*_?[0-9]_[0-9][/-]'
-                                      '.*_?([0-9]+)_([0-9]+)[/-]'
-                                      '[tT][xX]_([0-9]+).*').match, pose_id_lines))
-    # print('POSE IDS', pose_ids[:5])
-
-    if not pose_ids:  # we don't have names generated from Nanohedra Docking
+    # # extract only pose_ids from the input pose_id_lines
+    # pose_ids = list(filter(re.compile('.*(\w{4})_(\w{4})[/-]'
+    #                                   '.*_?[0-9]_[0-9][/-]'
+    #                                   '.*_?([0-9]+)_([0-9]+)[/-]'
+    #                                   '[tT][xX]_([0-9]+).*').match, pose_id_lines))
+    # # print('POSE IDS', pose_ids[:5])
+    pose_ids = pose_id_lines
+    if not pose_ids:  # Don't have names generated from Nanohedra Docking
         pose_ids = pose_id_lines
         print(pose_ids[:5])
         # raise NotImplementedError('The format_pose_ids tool needs to be adapted for non-Nanohedra docking entries')
@@ -114,4 +117,4 @@ if __name__ == '__main__':
         # print('GROUPS', final_groups[:5])
         # print('FINAL POSE IDS', final_pose_ids[:5])
 
-    utils.io_save(final_pose_ids)
+    utils.io_save(final_pose_ids, file_name=args.output_file)
