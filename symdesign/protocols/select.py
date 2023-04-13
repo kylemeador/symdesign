178917219ff281c7da4d99ec832dccfdd234107b
kylemeador <kylemeador@g.ucla.edu>
2023-04-12 18:27:07 -0700

    increase alignment_length

--- symdesign/protocols/select.py
+++ symdesign/protocols/select.py
@@ -1948,7 +1948,7 @@ def sql_sequences(pose_jobs: list[PoseJob]) -> list[PoseJob]:
         logger.info(f"Using the sequence '{tag_linker}' to link each protein sequence and the specified tag")
 
     # Format sequences for expression
-    alignment_length = 20
+    alignment_length = 40
     metrics_sequences = {}
     tag_sequences = {}
     final_sequences = {}
