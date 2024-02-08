1d0c6579249f878f00e80c011de6efb564e027ca
kylemeador <kylemeador@g.ucla.edu>
2024-02-08 00:24:09 -0800

    fix: module listing

--- symdesign/__init__.py
+++ symdesign/__init__.py
@@ -1,12 +1,15 @@
-"""Model and design proteins with inherent consideration for local and global symmetry
+"""
+#### Model, design, and analyze proteins without explicit considerations for applying local and global symmetry.
 
 Modules exported by this package:
 
-- `metrics`: Perform calculations on a protein pose
-- `protocols`: Implement a defined set of instructions for a protein pose
-- `resources`: Common methods and variable to connect job instructions to protocol implementation during job runtime
-- `sequence`: Handle biological sequences as python objects
-- `structure`: Handle biological structures as python objects
-- `third_party`: External dependencies that are installed by this package
-- `utils`: Miscellaneous functions, methods, and tools for all modules
+- [flags][flags]: Setup program inputs to perform a job
+- [metrics][metrics]: Perform calculations on a protein pose
+- [protocols][protocols]: Implement a defined set of instructions for a protein pose
+- [resources][resources]: Common methods and variable to connect job instructions to protocol implementation during job runtime
+- [sequence][sequence]: Handle biological sequences as python objects
+- [structure][structure]: Handle biological structures as python objects
+- [tools][tools]: Helper scripts, not quite yet a [protocols][protocols] worthy module, perhaps only accomplishes a sinlge task like file manipulation
+- [utils][utils]: Miscellaneous functions, methods, and tools for all modules
+- [visualization][visualization]: Miscellaneous PyMol visualization helper functions and plotting config.
 """
